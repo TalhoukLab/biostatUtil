da1bfdd03f6d43c277520efdc1914960eb99c087
Derek Chiu <dchiu@stat.ubc.ca>
2015-05-12 16:15:32 -0700

    Added rowPercentHTML

--- nice_frequencies-Derek.R
+++ nice_frequencies-Derek.R
@@ -7,7 +7,7 @@ kappaCIboot<-function(x,y,seed=20,num.boot=1000,conf.level=0.95){
   require(psy)
   set.seed(seed)
 
-  #Function to compute cohen's kappa with binary data and `conf.level`% CI based on `num.boot` bootstrap samples
+  # Function to compute cohen's kappa with binary data and `conf.level`% CI based on `num.boot` bootstrap samples
   ckappa.boot <- function(data,x) {ckappa(data[x,])[[2]]}
   res <- boot(cbind(x,y),ckappa.boot,num.boot)
   bootCI <- boot.ci(res,type = "bca",conf = conf.level)   # adjusted bootstrap percentile (BCa) confidence interval (better)
@@ -76,6 +76,55 @@ row.percent <- function(t, pretty.text=FALSE, digits=4) {
     )
   } 
   return(t/apply(t,1,sum))
+}
+
+row.percent.as.html <- function(
+  t, 
+  show.count=FALSE, # whether to show the count
+  # pretty.text=FALSE, digits=4, # these are part of ... i.e. argment for row/col.percent
+  row.names=NULL,
+  column.names=NULL,
+  html.table.border=0,
+  banded.rows=FALSE,
+  css.class.name.odd="odd",
+  css.class.name.even="even",...
+) {
+  col.th.style <- "COL.TH.STYLE"
+  row.th.style <- "ROW.TH.STYLE"
+  table.values <- row.percent(t, pretty.text=pretty.text, digits=digits,...)
+  result <- paste("<table border=",html.table.border,">",sep="")
+  # print header
+  if (!is.null(column.names)) {
+    colnames(table.values) <- column.names
+  }
+  if (!is.null(row.names)) {
+    rownames(table.values) <- row.names
+  } else {
+    row.names <- rownames(table.values)
+  }
+  result <- paste(result,"<tr><th style='",col.th.style,"'>",paste(names(table.values),collapse=paste("</th><th style='",col.th.style,"'>",sep="")),"</th></tr>",sep="")
+  
+  # print values
+  if (length(dim(table.values))==1) {
+    if (show.count) {
+      result <- paste(result,"<tr><td>",paste(paste(t," (",table.values,")",sep=""),collapse="</td><td>"),"</td></tr>",sep="")
+    } else {
+      result <- paste(result,"<tr><td>",paste(table.values,collapse="</td><td>"),"</td></tr>",sep="")
+    }
+  } else {
+    # for tables with > 1 row, there will be need to be row names
+    for (i in 1:nrow(table.values)) {
+      tr.class <- ifelse(banded.rows,paste(" class='",ifelse(i%%2==0,css.class.name.even,css.class.name.odd),"'",sep=""),"")
+      result <- paste(result,"<tr",tr.class,"><th style='",row.th.style,"'>",row.names[i],"</th>",sep="")
+      if (show.count) {
+        result <- paste(result,"<td>",paste(paste(t[i,]," (",table.values[i,],")",sep=""),collapse="</td><td>"),"</td></tr>",sep="")	
+      } else {
+        result <- paste(result,"<td>",paste(table.values[i,],collapse="</td><td>"),"</td></tr>",sep="")
+      }
+    }
+  }
+  result <- paste(result,"</table>",sep="")
+  return(result)
 }
 
 #########################################################
