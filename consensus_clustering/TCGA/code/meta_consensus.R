f3a15ddaca551543e6e103973034345cfd1428af
Derek Chiu <dchiu@bccrc.ca>
2015-05-27 16:09:14 -0700

    added mutual information and confusion matrix

--- consensus_clustering/TCGA/code/meta_consensus.R
+++ consensus_clustering/TCGA/code/meta_consensus.R
@@ -5,24 +5,14 @@ NMF.clusts <- readRDS("consensus_clustering/TCGA/outputs/classes_NMF.rds")
 all.clusts <- cbind(CCP.clusts, NMF.clusts)
 meta.clusts <- consensusMatrix(all.clusts)
 
+# Heatmaps
 library(RColorBrewer)
 BuPuFun <- colorRampPalette(brewer.pal(n = 9, "BuPu"))
 paletteSize <- 256
 palBuPu <- BuPuFun(paletteSize)
-
-# Heatmaps
 heatmap(meta.clusts, labRow = NA, labCol = NA, col = palBuPu)
 
-# Compare final clustering with TCGA
-final.compare <- hclust(dist(meta.clusts), method = "average") %>%
-  cutree(4) %>%
-  as.factor %>%
-  set_names(hclust(dist(meta.clusts), method = "average")$labels) %>%
-  set_names(substring(names(.), first = 18)) %>%
-  table(., names(.)) %>%
-  extract(names(sort(apply(., 1, which.max))), ) %>%
-  set_rownames(colnames(.)) %>%
-  as.table
-
-names(dimnames(final.compare)) <- c("Predicted", "Reference")
-confusionMatrix(final.compare)
+# Proportion at least 0.6
+high.pairs <- (sum(meta.clusts >= 0.6) - ncol(meta.clusts)) / 2
+total.pairs <- choose(ncol(meta.clusts), 2)
+high.pairs / total.pairs
--- consensus_clustering/TCGA/code/performance_measures.R
+++ consensus_clustering/TCGA/code/performance_measures.R
@@ -1,15 +1,31 @@
 library(mclust)
+library(entropy)
 library(caret)
-library(ROCR)
+library(infotheo)
 
 CCP.clusts <- readRDS("consensus_clustering/TCGA/outputs/classes_CCP.rds")
 NMF.clusts <- readRDS("consensus_clustering/TCGA/outputs/classes_NMF.rds")
 all.clust <- cbind(CCP.clusts, NMF.clusts) %>%
   set_rownames(substring(rownames(.), first = 18))
+meta.clusts <- consensusMatrix(all.clusts)
+
+# Confusion Matrix
+final.compare <- hclust(dist(meta.clusts), method = "average") %>%
+  cutree(4) %>%
+  as.factor %>%
+  set_names(hclust(dist(meta.clusts), method = "average")$labels) %>%
+  set_names(substring(names(.), first = 18)) %>%
+  table(., names(.)) %>%
+  extract(names(sort(apply(., 1, which.max))), ) %>%
+  set_rownames(colnames(.)) %>%
+  as.table
+
+names(dimnames(final.compare)) <- c("Predicted", "Reference")
+confusionMatrix(final.compare)
 
 # Adjusted Rand Index
 sort(apply(all.clust, 2, adjustedRandIndex, x = rownames(all.clust)))
 
-# 
-# hc.final <- hclust(dist(meta.clusts), method = "average")
-# prediction(predictions = cutree(hc.final, 4), labels = substring(hc.final$labels, first = 18))
+# Mutual Information
+sort(apply(all.clust, 2, mutinformation, Y = rownames(all.clust)))
+mi.plugin(final.compare)
