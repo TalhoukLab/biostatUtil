b72eec675bbac547512b2cb58c6f68294c4275de
Derek Chiu <dchiu@bccrc.ca>
2015-05-28 17:24:57 -0700

    modified code for PAC

--- consensus_clustering/TCGA/code/algorithms_CCP.R
+++ consensus_clustering/TCGA/code/algorithms_CCP.R
@@ -21,51 +21,44 @@ dat <- dat.raw %>%
 # user  system elapsed 
 # 47.837  25.207  72.141 
 hc.euc <- ConsensusClusterPlus(dat, maxK = 4, reps = 1000, pItem = 0.8, seed = 123, verbose = T)
-hc.euc.tab <- hc.euc[[4]]$consensusClass
 
 # user  system elapsed 
 # 405.932  28.070 432.125 
 hc.diana <- ConsensusClusterPlus(dat, maxK = 4, reps = 1000, pItem = 0.8, clusterAlg = "dianaHook", seed = 123, verbose = T)
-hc.diana.tab <- hc.diana[[4]]$consensusClass
 
 # user  system elapsed 
 # 164.093  15.505 177.808 
 km.euc <- ConsensusClusterPlus(dat, maxK = 4, reps = 1000, pItem = 0.8, clusterAlg = "kmdist", distance = "euclidean",
                                seed = 123, verbose = T)
-km.euc.tab <- km.euc[[4]]$consensusClass
+# km.euc.tab <- km.euc[[4]]$consensusClass
 
 # user  system elapsed 
 # 165.402  14.873 178.425 
 km.spr <- ConsensusClusterPlus(dat, maxK = 4, reps = 1000, pItem = 0.8, clusterAlg = "kmdist", distance = "spearman",
                                   seed = 123, verbose = T)
-km.spr.tab <- km.spr[[4]]$consensusClass
 
 # user  system elapsed 
 # 156.912  17.560 172.940 
 km.min <- ConsensusClusterPlus(dat, maxK = 4, reps = 1000, pItem = 0.8, clusterAlg = "kmdist", distance = "myMIdist",
                                   seed = 123, verbose = T)
-km.min.tab <- km.min[[4]]$consensusClass
 
 # user  system elapsed 
 # 115.549  24.418 138.091 
 pam.euc <- ConsensusClusterPlus(dat, maxK = 4, reps = 1000, pItem = 0.8, clusterAlg = "pam", distance = "euclidean",
                                    seed = 123, verbose = T)
-pam.euc.tab <- pam.euc[[4]]$consensusClass
 
 # user  system elapsed 
 # 136.183  23.754 158.044 
 pam.spr <- ConsensusClusterPlus(dat, maxK = 4, reps = 1000, pItem = 0.8, clusterAlg = "pam", distance = "spearman",
                                    seed = 123, verbose = T)
-pam.spr.tab <- pam.spr[[4]]$consensusClass
 
 # user  system elapsed 
 # 122.436  23.794 144.373 
 pam.min <- ConsensusClusterPlus(dat, maxK = 4, reps = 1000, pItem = 0.8, clusterAlg = "pam", distance = "myMIdist",
                                    seed = 123, verbose = T)
-pam.min.tab <- pam.min[[4]]$consensusClass
 
-# Save results into an R object
-cbind(hc.euc.tab, hc.diana.tab, km.euc.tab, km.spr.tab,
-      km.min.tab, pam.euc.tab, pam.spr.tab, pam.min.tab) %>%
-  set_colnames(c("hcAEucl","hcDianaEucl","kmEucl","kmSpear","kmMI","pamEucl","pamSpear","pamMI")) %>%
-  saveRDS("consensus_clustering/TCGA/outputs/CCP_classes.rds")
+# Save results
+list(hc.euc, hc.diana, km.euc, km.spr, km.min, pam.euc, pam.spr, pam.min) %>%
+  set_names(c("hcAEucl", "hcDianaEucl","kmEucl", "kmSpear", "kmMI",
+              "pamEucl", "pamSpear", "pamMI")) %>%
+  saveRDS("consensus_clustering/TCGA/outputs/results_CCP.rds")
--- consensus_clustering/TCGA/code/performance_measures.R
+++ consensus_clustering/TCGA/code/performance_measures.R
@@ -4,17 +4,16 @@ library(entropy)
 library(caret)
 library(infotheo)
 
-CCP.clusts <- readRDS("consensus_clustering/TCGA/outputs/classes_CCP.rds")
-NMF.clusts <- readRDS("consensus_clustering/TCGA/outputs/classes_NMF.rds")
-all.clust <- cbind(CCP.clusts, NMF.clusts) %>%
-  set_rownames(substring(rownames(.), first = 18))
-meta.clusts <- consensusMatrix(all.clusts)
+all.clust <- readRDS("consensus_clustering/TCGA/outputs/results_CCP.rds") %>%
+  sapply(., function(x) x[[4]]$consensusClass) %>%
+  cbind(., readRDS("consensus_clustering/TCGA/outputs/classes_NMF.rds"))
+meta.cm <- consensusMatrix(all.clust)
 
 # Confusion Matrix
-final.compare <- hclust(dist(meta.clusts), method = "average") %>%
+final.compare <- hclust(dist(meta.cm), method = "average") %>%
   cutree(4) %>%
   as.factor %>%
-  set_names(hclust(dist(meta.clusts), method = "average")$labels) %>%
+  set_names(hclust(dist(meta.cm), method = "average")$labels) %>%
   set_names(substring(names(.), first = 18)) %>%
   table(., names(.)) %>%
   extract(names(sort(apply(., 1, which.max))), ) %>%
@@ -25,22 +24,27 @@ names(dimnames(final.compare)) <- c("Predicted", "Reference")
 confusionMatrix(final.compare)
 
 # Adjusted Rand Index
-sort(apply(all.clust, 2, adjustedRandIndex, x = rownames(all.clust)))
+sort(apply(all.clust, 2, adjustedRandIndex, x = substring(rownames(all.clust), first = 18)))
 
 # Mutual Information
-sort(apply(all.clust, 2, mutinformation, Y = rownames(all.clust)))
+sort(apply(all.clust, 2, mutinformation, Y = substring(rownames(all.clust), first = 18)))
 mi.plugin(final.compare)
 
-# Cohen's kappa and Fleiss' kappa
+# Fleiss' kappa: unweighted/weighted
 wkappa(final.compare)
 
 # CDF
 test <- readRDS(file = "consensus_clustering/TCGA/outputs/nmf_output_05-19-2015.rds")
 nmf.div <- consensusMatrix(dat = test[, , 1])
+nmf.eucl <- consensusMatrix(dat = test[, , 2])
 qplot(nmf.div[lower.tri(nmf.div)], stat = "ecdf", geom = "line")
 
 qplot(km.euc[[4]]$consensusMatrix[lower.tri(km.euc[[4]]$consensusMatrix)], stat = "ecdf", geom = "step")
 
 # PAC
-nmf.div.PAC <- PAC(nmf.div)
-km.euc.PAC <- PAC(km.euc[[4]]$consensusMatrix)
+PAC(nmf.div)
+PAC(nmf.eucl)
+
+PAC.all <- readRDS("consensus_clustering/TCGA/outputs/results_CCP.rds") %>%
+  sapply(., function(x) PAC(x[[4]]$consensusMatrix)) %>%
+  sort
