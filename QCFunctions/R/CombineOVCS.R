ae3b6e18956ec278fc1adead6dffaa60467d517a
Aline Talhouk <atalhouk@bccrc.ca>
2015-06-11 10:22:26 -0700

    Added Package QCFunctions

---
+++ QCFunctions/R/CombineOVCS.R
@@ -0,0 +1,39 @@
+#' Combines Ovarian Codeset 1 and Codeset 2 Nanostring Data
+#'
+#' This function matches the genes that are common to codeset 1 and codeset 2 (same accession number) and combines them after fixing nomenclature. 
+#' @param CS1 matrix of raw counts obtained from nCounter (rows are genes) with codeset 1. The first three columns must be labeled: c("Code.Class","Name","Accession") and contain that information.
+#' @param CS2 matrix of raw counts obtained from nCounter (rows are genes) with codeset 2. The first three columns must be labeled: c("Code.Class","Name","Accession") and contain that information.
+#' @return matrix of combined data with the first three columns labeled as c("Code.Class","Name","Accession")
+
+CombineOVCS <- function(CS1, CS2) {
+  CS1.c <- CS1[CS1$Accession %in% CS2$Accession, ]
+  CS2.c <- CS2[CS2$Accession %in% CS1$Accession, ]
+  CS1.c <- CS1.c[order(CS1.c$Accession), ]
+  CS2.c <- CS2.c[order(CS2.c$Accession), ]
+  
+  #remove ERBB3 from codeset 2
+  CS2.c <- CS2.c[!CS2.c$Name=="ERBB3", ]
+  
+  #rename SOX11 and LDHC in codeset1
+  CS1.c$Name[CS1.c$Name == "SOX11"] <- "LOX"
+  CS1.c$Name[CS1.c$Name == "LDHC_V2"] <- "LDHC"
+  
+  #Add the additional four genes with non-matching accession
+  AG <- c("DUSP4", "IGKC", "LDHA", "PTHLH")
+  CS1.c <- rbind(CS1.c, CS1[CS1$Name %in% AG, ])
+  CS2.c <- rbind(CS2.c, CS2[CS2$Name %in% AG, ])
+  
+  #Order the matrices by gene Name
+  CS1.c <- CS1.c[order(CS1.c$Name), ]
+  CS2.c <- CS2.c[order(CS2.c$Name), ]
+  
+  if (all(CS1.c$Name == CS2.c$Name)){
+    CS2temp <- CS2.c[, -(1:3)]
+    CS1temp <- CS1.c[, -(1:3)]
+    Combined <- cbind(CS1.c[, 1:3], CS1temp[, order(colnames(CS1temp))], CS2temp[, order(colnames(CS2temp))])
+    rownames(Combined) <- Combined$Name
+    return(Combined)
+  }
+  else
+  { print("Error: the gene names in the two sets don't match!") }
+}
---
+++ QCFunctions/R/NanoStringQC.R
@@ -0,0 +1,63 @@
+#' QC for NanoString Data
+#'
+#' This function computes NanoString quality control parameters and flags
+#' @param raw: matrix of raw counts obtained from nCounter (rows are genes). The first three columns must be labeled: c("Code.Class","Name","Accession") and contain that information.
+#' @param exp:  matrix of annotations with rows in the same order as the columns of raw.
+#' @param detect: Percent threshold of genes over lod that we would like to detect (not decimal).
+#' @param sn: Signal to noise ratio of the housekeeping genes we are willing to tolerate
+#' @param plots: Binary, indicates whether plots to visualise the results are requested, defaults to TRUE
+#' @param ttl: a string to show the title on the plots
+#' @param explore: returns the plots only, defaults to TRUE
+#' @return matrix of annotations updated with normalization parameters
+
+
+NanoStringQC <- function(raw, exp, detect = 80, sn = 150, plots = TRUE, ttl =" ", explore = TRUE){
+  genes = raw$Name; rownames (raw) = genes 
+  HKgenes = genes[raw$Code.Class == "Housekeeping"]
+  PCgenes = genes[raw$Code.Class == "Positive"]
+  NCgenes = genes[raw$Code.Class == "Negative"]
+  PCconc = sub("\\).*", "", sub(".*\\(", "", PCgenes)) 
+  PCconc = log(as.numeric(PCconc), base = 2)
+  lin = function(x){
+    if (any(x == 0)){
+      res = NA 
+    } else {
+      fit <- lm(log(x,base=2) ~ PCconc) 
+      res = summary(fit)$r.squared }
+    return(res)
+  }
+  
+  exp$linPC <- apply(raw[PCgenes, -(1:3)], 2, lin)
+  exp$linFlag <- exp$linPC < 0.9 | is.na(exp$linPC)
+  exp$perFOV <- exp$fov.counted / exp$fov.count
+  exp$imagingFlag <- (exp$perFOV < 0.75)
+  exp$lod <- apply(raw[NCgenes, -(1:3)], 2, mean) + 2 * (apply(raw[NCgenes, -(1:3)], 2, sd))
+  exp$llod <- apply(raw[NCgenes, -(1:3)], 2, mean) - 2 * (apply(raw[NCgenes, -(1:3)], 2, sd))
+  exp$lodFlag <- t(as.vector(raw["POS_E(0.5)", -(1:3)]) < exp$lod)
+  exp$gd <- apply(raw[, -(1:3)] > exp$lod, 2, sum) # Number of genes above background
+  exp$pergd <- (exp$gd / nrow(raw)) * 100
+  exp$averageHK <- apply(raw[HKgenes, -(1:3)], 2, mean)
+  exp$sn <- exp$averageHK / exp$lod
+  exp$sn[exp$lod < 0.001] <- 0 # This is so that it does not underflow
+  exp$bdFlag <- (exp$binding.density < 0.05 | exp$binding.density > 2.25)
+  
+  if (plots) {
+    par(mfrow = c(1,2))
+    plot(exp$sn, exp$pergd, pch=20, col="deepskyblue", xaxt="n", ylim=c(0,100), 
+         xlab = "Signal to Noise Ratio", ylab = "Ratio of Genes Detected")
+    axis(1, at = seq(0, max(exp$sn) + 1, 300))
+    abline(v = sn, col = "red", lwd = 2)
+    abline(h = detect, lty = 2)
+    
+    hist(exp$sn, 50, col = "cornsilk", prob = TRUE,
+         xlab = "Signal to Noise", ylab = "Probability", main = "")
+    abline(v = sn,lwd = 3)
+  }
+  title(ttl, outer = TRUE, line = -2)
+  exp$normFlag <- (exp$sn < sn | exp$pergd < detect)
+  exp$QCFlag <- as.vector(exp$lodFlag == TRUE | exp$normFlag == TRUE | exp$imagingFlag == TRUE | exp$linFlag == TRUE)
+  if (!explore)
+    return(exp$QCFlag)
+  else
+    return(exp)
+}
