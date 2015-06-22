0e1d3906eb45a24ce099da83806fe07acf8bbb06
Aline Talhouk <atalhouk@bccrc.ca>
2015-06-22 15:25:21 -0700

    Added ratioMethods to batchAdjust

---
+++ batchAdj/R/ratioMethod.R
@@ -0,0 +1,11 @@
+#' Batch adjustment by taking the ratio of a reference sample
+#' @param Y2 is the data run in the second batch, samples are rows and genes are columns
+#' @param R1 is the reference data run in the first batch
+#' @param R2 is the reference data run in the second batch
+#' @return Y2new is the Y2 data adjusted to batch 1
+#' @author Aline Talhouk
+ratioMethod <- function(Y2, R1, R2) {
+  m <- apply(R1, 2, mean) - apply(R2, 2, mean)
+  Y2new <- t(apply(Y2, 1, function(x){x + m}))
+  return(Y2new)
+}
