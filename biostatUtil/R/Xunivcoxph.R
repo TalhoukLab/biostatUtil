15f4f16c5cb81d67b4d0cec413801208e29f63f2
Derek Chiu <dchiu@bccrc.ca>
2015-06-22 16:18:58 -0700

    formatted spacing

--- biostatUtil/R/Xunivcoxph.R
+++ biostatUtil/R/Xunivcoxph.R
@@ -1,11 +1,14 @@
 #' Univariate cox proprtional hazards model
+#' @return Hazard ratio, confidence interval, and likelihood ratio test
+#' p-value
+#' @author Aline Talhouk
 #' @export
-Xunivcoxph <- function(mod, digits = 3, coxph.type = "coxph") {
-  #returns HR and CI and LR pval and events per n
+Xunivcoxph <- function(mod, coxph.type = "coxph", digits = 3) {
   switch(coxph.type,
          coxph = {
            HR <- round(summary(mod)$conf.int, digits)[, 1:2]
-           CI <- paste(round(summary(mod)$conf.int, digits)[, 3], round(summary(mod)$conf.int, digits)[, 4], sep = "-")
+           CI <- paste(round(summary(mod)$conf.int, digits)[, 3],
+                       round(summary(mod)$conf.int, digits)[, 4], sep = "-")
            if (is.null(dim(HR))) {
              HR <- HR[1] # when there are only two groups
            } else {
@@ -15,7 +18,8 @@ Xunivcoxph <- function(mod, digits = 3, coxph.type = "coxph") {
          },
          coxphf = {
            HR <- round(exp(mod$coefficients), digits)
-           CI <- paste(round(mod$ci.lower, digits), round(mod$ci.upper, digits), sep = "-")
+           CI <- paste(round(mod$ci.lower, digits),
+                       round(mod$ci.upper, digits), sep = "-")
            res <- paste0("HR(F) ", HR, " (95% CI, ", CI, ")")
          }
   )
