cc8d49254cf9c7cc2f504078288b8c80892cded9
Aline Talhouk <atalhouk@bccrc.ca>
2015-06-22 14:49:22 -0700

    Annotated and Style guide checked scattermat and DSC

--- batchAdj/R/DSC.R
+++ batchAdj/R/DSC.R
@@ -1,25 +1,30 @@
+#' Compute the Dispersion Separability Criterion (DSC) which indicates how similar two data sets are by considering the between data scatter matrix and the withing data scatter matrix
+#' @param data is a combined data set
+#' @param Y a vector indicating the different batches
+#' @param permut, the number of permutations to compute a pvalue for DSC using a permutation test.
+#' @author Aline Talhouk
 
-DSC<-function(data,Y){
-  scatmat=scattermat(data,Y)
-  DSC.observed=sqrt(sum(diag(scatmat$B)))/sqrt(sum(diag(scatmat$W)))
-  
-  number_of_permutations = 1000
-  
-  DSC.random = NULL
+DSC <- function(data, Y, permut=10) {
+  scatmat <- scattermat(data, Y)
+  DSC.observed <- sqrt(sum(diag(scatmat$B))) / sqrt(sum(diag(scatmat$W)))
+
+  number_of_permutations <- permut
+
+  DSC.random <- NULL
   for (i in 1 : number_of_permutations) {
-    levs=levels(factor(Y))
+    levs <- levels(factor(Y))
     # Combine the two datasets into a single dataset
     # i.e., under the null hypothesis, there is no difference between the two groups
     # Sample from the combined dataset
-    a.random = data[sample (dim(data)[1], sum(Y==levs[1]), TRUE),]
-    b.random = data[sample (dim(data)[1], sum(Y==levs[2]), TRUE),]
-    data.random=rbind(a.random,b.random)
+    a.random <- data[sample (dim(data)[1], sum(Y == levs[1]), TRUE), ]
+    b.random <- data[sample (dim(data)[1], sum(Y == levs[2]), TRUE), ]
+    data.random <- rbind(a.random, b.random)
     # Null (permuated) difference
-    rand.scat=scattermat(data.random,Y)
-    DSC.random[i]=sqrt(sum(diag(rand.scat$B)))/sqrt(sum(diag(rand.scat$W)))
+    rand.scat <- scattermat(data.random, Y)
+    DSC.random[i] <- sqrt(sum(diag(rand.scat$B))) / sqrt(sum(diag(rand.scat$W)))
   }
   # P-value is the fraction of how many times the permuted difference is equal or more extreme than the observed difference
-  
-  pvalue = sum(abs(DSC.random) >= abs(DSC.observed)) / number_of_permutations
-  return(list(DSC=round(DSC.observed,3),pval=pvalue))
+
+  pvalue <- round(sum(abs(DSC.random) >= abs(DSC.observed)) / number_of_permutations,4)
+  return(list(DSC = round(DSC.observed, 3), pval = pvalue))
 }
--- batchAdj/R/scattermat.R
+++ batchAdj/R/scattermat.R
@@ -3,12 +3,13 @@
 #' @param W is the withing class scatter matrix
 #' @param Y is a vector of classes
 #' @author Aline Talhouk
+#'
 scattermat <- function(data, Y) {
   l <- dim(data)[2]        #CALCULATE SIZE OF DATA
   clases <- unique(Y)       #GET VECTOR OF CLASSES
   tot_clases <- length(clases) #HOW MANY CLASSES
   B <- matrix(0, nrow = l, ncol = l)   #INIT B AND W
-  W <- matrix(0, nrow = l, ncol = l)         
+  W <- matrix(0, nrow = l, ncol = l)
   overallmean <- apply(data, 2, mean)    #MEAN OVER ALL DATA
   for (i in 1:tot_clases) {
     clasei <- which(Y == clases[i]) #GET DATA FOR EACH CLASS
