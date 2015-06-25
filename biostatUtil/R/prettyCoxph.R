e231d01887a1ce38ef5e2698b5e14c0d75017412
Derek Chiu <dchiu@bccrc.ca>
2015-06-25 16:53:21 -0700

    more documentation for prettyCoxph

--- biostatUtil/R/prettyCoxph.R
+++ biostatUtil/R/prettyCoxph.R
@@ -1,4 +1,32 @@
-#' Coxph with nicer output
+#' Nicer outputs from coxph model fits
+#' 
+#' Formats the coxph (or coxphf) model outputs into a prettier display.
+#' 
+#' The original fit call is returned, along with other useful statistics.
+#' 
+#' @param input.formula a formula object, with the response on the left of a
+#' \code{~} operator, and the terms on the right. The response must be a
+#' survival object as returned by the \code{Surv} function.
+#' @param input.d data.frame containing the variables in \code{input.formula}
+#' @param use.firth if set to -1, Firth's correction is applied using
+#' \code{coxphf}. The default value is 1, which uses \code{coxph}.
+#' @param check.ph if \code{TRUE}, checks the proportional hazard assumption.
+#' @param ph.test.plot.filename filename of residual plot. If set to
+#' \code{no.file} (default), plots to console. If \code{NA}, no plot will be
+#' generated.
+#' @return A list with elements
+#' \item{output}{A character matrix showing the hazard ratio, confidence bounds,
+#' and p-value for each coefficient. If \code{check.ph = TRUE}, the last column
+#' shows the results of the PH test, otherwise it reads "NOT CALCULATED" for
+#' every coefficient.}
+#' \item{fit}{The fit output call returned from \code{coxph} or \code{coxphf}}
+#' \item{n}{The number of observations used in the fit}
+#' \item{nevent}{The number of events used in the fit}
+#' \item{ph.test}{The rho, Chi-squared statistic, and p-value for each coefficient.
+#' Not shown if \code{check.ph = FALSE}}
+#' \item{used.firth}{logical; if \code{TRUE}, Firth's correction was applied
+#' using \code{coxphf}}
+#' @author Samuel Leung, Derek Chiu
 #' @export
 prettyCoxph <- function(input.formula, input.d, use.firth = 1,
                         check.ph = FALSE,
@@ -65,15 +93,15 @@ prettyCoxph <- function(input.formula, input.d, use.firth = 1,
   if (ok.to.use.firth) {
     # coxphf fit object
     result <- cbind(
-      matrix(cbind(exp(fit.firth$coefficients),fit.firth$ci.lower,fit.firth$ci.upper), 
-             nrow=length(names(fit.firth$coefficients)), 
-             ncol=3, 
-             dimnames=c(list(names(fit.firth$coefficients)),list(c('exp(coef)','lower .95', 'upper .95')))
+      matrix(cbind(exp(fit.firth$coefficients), fit.firth$ci.lower,fit.firth$ci.upper), 
+             nrow = length(names(fit.firth$coefficients)), 
+             ncol = 3, 
+             dimnames = c(list(names(fit.firth$coefficients)), list(c('exp(coef)', 'lower .95', 'upper .95')))
       ),
-      matrix( fit.firth$prob,
-              nrow=length(names(fit.firth$coefficients)), 
-              ncol=1,
-              dimnames=c(list(names(fit.firth$coefficients)),list(c('Pr(>|z|)')))
+      matrix(fit.firth$prob,
+             nrow = length(names(fit.firth$coefficients)), 
+             ncol = 1,
+             dimnames = c(list(names(fit.firth$coefficients)), list(c('Pr(>|z|)')))
       ),
       ph.check
     )	
@@ -84,7 +112,7 @@ prettyCoxph <- function(input.formula, input.d, use.firth = 1,
              nrow = length(names(fit$coefficients)), 
              ncol = 3, 
              dimnames = c(list(names(fit$coefficients)),
-                          list(c('exp(coef)','lower .95', 'upper .95')))),
+                          list(c('exp(coef)', 'lower .95', 'upper .95')))),
       matrix(summary(fit)$coefficients[, 'Pr(>|z|)'],
              nrow = length(names(fit$coefficients)), 
              ncol = 1,
@@ -100,7 +128,7 @@ prettyCoxph <- function(input.formula, input.d, use.firth = 1,
     return.obj        <- list(result,   fit,   fit.firth, fit$n, fit$nevent, ph.test,  ok.to.use.firth)
     names(return.obj) <- c(   "output", "fit", "fit.firth", "n",   "nevent",   "ph.test", "used.firth")
   } else {
-    return.obj        <- list(result,   fit,   fit.firth,fit$n, fit$nevent,ok.to.use.firth)
+    return.obj        <- list(result,   fit,   fit.firth, fit$n, fit$nevent, ok.to.use.firth)
     names(return.obj) <- c(   "output", "fit", "fit.firth", "n",   "nevent"  ,"used.firth")
   }
   return(return.obj)
