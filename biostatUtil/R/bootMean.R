bababbd1ce9500dc0ffccb0b5a9dcaf10005e367
Derek Chiu <dchiu@bccrc.ca>
2015-06-25 16:09:30 -0700

    documentation for bootMean and coxphOut

--- biostatUtil/R/bootMean.R
+++ biostatUtil/R/bootMean.R
@@ -1,12 +1,34 @@
-#' Bootstraped mean
-#' @param x a vector
+#' Mean and bootstrap confidence interval 
+#' 
+#' Finds mean of a vector, with bootstrapped confidence bounds.
+#' 
+#' Takes a numeric vector and resamples with replacement \code{num.boot} times.
+#' If the input vector has any \code{NA} entries, include the argument
+#' \code{na.rm = TRUE} from \code{mean}.
+#' 
+#' @param x a vector (or matrix)
 #' @param num.boot number of bootstrap samples. Defaults to 1000.
 #' @param conf.level confidence level.
 #' @param random.seed random seed for resampling
 #' @param ... additional arguments to \code{mean}
-#' @return A bootstraped mean.
-#' @author Aline Talhouk
+#' @return A list with elements
+#' \item{obs.mean}{mean}
+#' \item{ci}{bootstraped confidence interval}
+#' \item{n}{number of bootstrap samples used}
+#' @author Aline Talhouk, Derek Chiu
 #' @export
+#' @examples 
+#' ## Vectors
+#' set.seed(344)
+#' bootMean(rnorm(100, 5, 3))
+#' bootMean(rnorm(100, 5, 3), num.boot = 500)
+#' bootMean(rnorm(100, 4, 3), conf.level = 0.90)
+#' 
+#' ## Missing Values
+#' set.seed(344)
+#' s <- ifelse(rexp(100, 0.5) < 1, NA, rexp(100, 0.5))
+#' bootMean(s)  # doesn't work
+#' bootMean(s, na.rm = T)
 bootMean <- function(x, num.boot = 1000, conf.level = 0.95, random.seed = 12, ...){
   set.seed(random.seed)
   obs.mean <- mean(x, ...)
--- biostatUtil/R/coxphOut.R
+++ biostatUtil/R/coxphOut.R
@@ -1,19 +1,43 @@
-#' coxphOut function
+#' Nice output from Cox regression object
+#' 
+#' Nicely formatted output from a Cox regression object,
+#' either \code{coxph} or \code{coxphf}.
+#' 
+#' For objects of class \code{coxphf}, the calculation of the number of events used
+#' in the fit is slightly different.
+#' 
+#' @param object a model fit object returned from \code{coxph} or \code{coxphf}
+#' @param coefnames a vector of labels for the coefficient names returned by the fit.
+#' \code{NULL} by default, uses original coefficient names.
+#' @param digits number of digits to round to
+#' @return A matrix with a row for each coefficient, and a column for each of the following
+#' elements
+#' \item{n}{the number of observations used in the fit.}
+#' \item{events}{the number of events used in the fit.}
+#' \item{coef}{log hazard ratio}
+#' \item{se}{standard error of \code{coef}}
+#' \item{Z-Score}{Wald test statistic}
+#' \item{P-value}{p-value for \code{Z-Score}}
+#' \item{HR}{hazard ratio}
+#' \item{Lower CI Limit}{Defaults to \code{2.5 \%}}
+#' \item{Upper CI Limit}{Defaults to \code{97.5 \%}}
+#' @author Aline Talhouk, Derek Chiu
 #' @export
-coxphOut <- function (x, coefnames = NULL, digits = 2, use.firth = FALSE) {
-  cox <- x  # this can be either an object from coxph() or coxphf() ... slightly different output
+coxphOut <- function (object, coefnames = NULL, level = 0.95, digits = 2) {
+  cox <- object
   Coef <- cox$coef
   se <- sqrt(diag(cox$var))
-  CI <- exp(confint(cox))
-  n <- cox$n  # summary(cox)$n
-  events <- ifelse(use.firth, sum(cox$y[, "status"]), cox$nevent)  # events <- summary(cox)$nevent
-  Z <- Coef/se
-  p <- 1 - pchisq((Coef / se)^2, 1)
+  CI <- exp(confint(cox, level = level))
+  n <- cox$n
+  events <- ifelse("coxphf" %in% class(cox),
+                   sum(cox$y[, "status"]), cox$nevent)
+  Z <- Coef / se
+  p <- 1 - pchisq(Z^2, 1)
   tmp <- cbind(n, events, Coef, se, Z, p, HR = exp(Coef), CI)
   if(is.null(coefnames))
     coefnames = names(Coef)
   dimnames(tmp) <- list(coefnames, c("n", "events" , "coef ", " se ",
                                      " Z-Score ", " P-value ", " HR ",
-                                     " 2.5% ", " 97.5% "))
+                                     colnames(CI)))
   return(round(tmp, digits))  
 }
