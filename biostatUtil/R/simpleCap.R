1e9eca67316704d2abfab60229ca30c1926cb181
Derek Chiu <dchiu@bccrc.ca>
2015-06-26 11:14:50 -0700

    simpleCap documentation

--- biostatUtil/R/simpleCap.R
+++ biostatUtil/R/simpleCap.R
@@ -1,11 +1,26 @@
 #' Simple capitalization
+#' 
+#' Capitalize the first letter of every word in a character string.
+#' 
+#' To capitalize only the first word, use \code{first.only = TRUE}.
+#' 
+#' @param x character string
+#' @param first.only logical; if \code{TRUE}, only the first word
+#' will be capitalized
+#' @return A character string with every word's first letter capitalized.
+#' @author Samuel Leung
+#' @references http://stackoverflow.com/questions/6364783/capitalize-the-first-letter-of-both-words-in-a-two-word-string
 #' @export
-simpleCap <- function(x, first.word.only = FALSE) {
-  if (!first.word.only) {
+#' @examples 
+#' simpleCap("clear cell")
+#' simplecap("high grade serous carcinoma")
+#' simpleCap("ovarian cancer", first.only = TRUE)
+simpleCap <- function(x, first.only = FALSE) {
+  if (!first.only) {
     s <- strsplit(x, " ")[[1]]
   } else {
     s <- x
   }
-  return(paste(toupper(substring(s, 1, 1)), substring(s, 2),
-               sep = "", collapse =" "))
+  return(paste0(toupper(substring(s, 1, 1)), substring(s, 2),
+                collapse =" "))
 }
