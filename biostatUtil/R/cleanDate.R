d4cff7f06cd689836399a14f8be7fd87197ac2c7
Derek Chiu <dchiu@bccrc.ca>
2015-07-02 11:00:17 -0700

    changed and documented cleanDate

--- biostatUtil/R/cleanDate.R
+++ biostatUtil/R/cleanDate.R
@@ -1,67 +1,75 @@
 #' Clean dates
-#' @author Samuel Leung
+#' 
+#' Clean a date and reformat it to another style.
+#' 
+#' @param x a date string or a numeric representation of a date
+#' (e.g. January 13th, 1991 would be 19910113)
+#' @param original.format format of input \code{x}
+#' @param preferred.format format to change \code{x} to
+#' @param existing.missing.codes missing dates
+#' @param return.missing.codes what to return if there is a missing input
+#' @param ... additional arguments to \code{\link{formatDate}}
+#' @return A date string cleaned and formatted from the original (unformatted)
+#' date
+#' @author Samuel Leung, Derek Chiu
 #' @export
+#' @examples 
+#' cleanDate("09/11/1991", original.format = "MM.DD.YYYY", preferred.format = "DD.MM.YYYY")
+#' cleanDate(11091991, original.format = "DD.MM.YYYY", preferred.format = "YYYY.MMM.DD")
+#' cleanDate(11091991, original.format = "DD.MM.YYYY", preferred.format = "YYYY.MMM.DD", sep = "-")
 cleanDate <- function(x, original.format, preferred.format,
-                      existing.missing.codes = "", return.missing.code = NA) {
+                      existing.missing.codes = "", return.missing.code = NA,
+                      ...) {
   if (is.na(x))
     return(return.missing.code)
   x <- trimws(x)
   if (x %in% existing.missing.codes)
-    return(x)  # preserve error coding
-  # need to break the date string into components 
+    return(x)
   date.comp <- strsplit(x, "/|-")[[1]]
   temp <- suppressWarnings(as.numeric(date.comp[1]))
   if (is.na(temp)) {
-    # cat("WARNING (clean.date.string.and.format): trying to format a non-standard date string: ",x,"\n")
     return(return.missing.code)
   }
-  if (temp > 1000000) {
-    # this would a format of yyyymmdd or ddmmyyyy or mmddyyyy
-    temp <- paste0(ifelse(temp < 10000000, "0", ""), temp)  # 7 or 8-digits; turn temp back to string
-    if (as.numeric(substr(temp, 5, 6)) < 13) {  # assume we will NOT be looking at any dates from 1200's!!!!
-      # must be YYYYMMDD
+  if (temp > 1000000) {  # yyyymmdd or ddmmyyyy or mmddyyyy
+    temp <- paste0(ifelse(temp < 10000000, "0", ""), temp)  # pad leading 0 for jan-sept; turn temp back to string
+    if (original.format == "DD.MM.YYYY") {
+      return(formatDate(substr(temp, 1, 2), substr(temp, 3, 4),
+                        substr(temp, 5, 8), date.format = preferred.format,
+                        ...))
+    } else if (original.format == "MM.DD.YYYY") {
+      return(formatDate(substr(temp, 3, 4), substr(temp, 1, 2),
+                        substr(temp, 5, 8), date.format = preferred.format,
+                        ...))
+    } else if (original.format == "YYYY.MM.DD") {
       return(formatDate(substr(temp, 7, 8), substr(temp, 5, 6),
-                        substr(temp, 1, 4), date.format = preferred.format))
-    } else if (as.numeric(substr(temp, 1, 2)) > 12) {
-      # must be DDMMYYYY
-      return(formatDate(substr(temp, 1, 2), substr(temp, 3, 4),
-                        substr(temp, 5, 8), date.format = preferred.format))
-    } else if (as.numeric(substr(temp, 3, 4)) > 12) {
-      # must be MMDDYYYY
-      return(formatDate(substr(temp, 3, 4), substr(temp, 1, 2),
-                        substr(temp, 5, 8), date.format = preferred.format))
-    } else if (original.format == DDMMYYYY) {
-      return(formatDate(substr(temp, 1, 2), substr(temp, 3, 4),
-                        substr(temp, 5, 8), date.format = preferred.format))
+                        substr(temp, 1, 4), date.format = preferred.format,
+                        ...))
     } else {
-      # assume must be MMDDYYYY
-      return(formatDate(substr(temp, 3, 4), substr(temp, 1, 2),
-                        substr(temp, 5, 8), date.format = preferred.format))
+      stop('ERROR (cleanDate): original.format must be one of
+           "DD.MM.YYYY", "MM.DD.YYYY", or "YYYY.MM.DD".')
+      return(NA)
     }
-  } else if (temp > 31) {
-    # must be YYYY/MM/DD because no day can be > 31
+  } else if (temp > 31) {  # must be YYYY/MM/DD
     return(formatDate(date.comp[3], date.comp[2], date.comp[1],
-                      date.format = preferred.format))
-  } else if (temp > 12) {
-    # must be DD/MM/YYYY
+                      date.format = preferred.format, ...))
+  } else if (temp > 12) {  # must be DD/MM/YYYY
     return(formatDate(date.comp[1], date.comp[2], date.comp[3],
-                      date.format = preferred.format))
-  } else { # first component is <= 12 ... however, we are not sure if it refers to a day or month
+                      date.format = preferred.format, ...))
+  } else { # first component <= 12 ... however, we are not sure if it refers to a day or month
     temp <- as.numeric(date.comp[2]) # second component can either be a day or month
-    if (temp > 12) {
-      # must be MM/DD/YYYY
+    if (temp > 12) {   # must be MM/DD/YYYY
       return(formatDate(date.comp[2], date.comp[1], date.comp[3],
-                        date.format = preferred.format))
-    } else { # BOTH first and second component is <=12
-      # assume its in the original format
-      if (original.format == MM.DD.YYYY) {
+                        date.format = preferred.format, ...))
+    } else {  # BOTH first & second component <=12; assume original.format
+      if (original.format == "MM.DD.YYYY") {
         return(formatDate(date.comp[2], date.comp[1], date.comp[3],
-                          date.format = preferred.format))
-      } else if (original.format == DD.MM.YYYY) {
+                          date.format = preferred.format, ...))
+      } else if (original.format == "DD.MM.YYYY") {
         return(formatDate(date.comp[1], date.comp[2], date.comp[3],
-                          date.format = preferred.format))
+                          date.format = preferred.format, ...))
       } else {
-        cat("ERROR (clean.date.string.and.format): unknown original.format specified: ", original.format, "\n")
+        stop('ERROR (cleanDate): unknown original.format specified: ',
+             original.format)
         return(NA)
       }
     }
