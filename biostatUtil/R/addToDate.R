ca1cbe824c7480fd221f6e229d1023eb976c653e
Derek Chiu <dchiu@bccrc.ca>
2015-07-03 13:59:07 -0700

    documented addtoDate and compareDate

--- biostatUtil/R/addToDate.R
+++ biostatUtil/R/addToDate.R
@@ -1,12 +1,25 @@
 #' Add to a date
+#' 
+#' Adds a period of time to a date string and returns a new date string.
+#' 
+#' The added period of time can be in \code{"days"}, \code{"months"},
+#' or \code{"years"}. If \code{delta} is negative, then the returned date
+#' will be earlier than \code{org.date}. The output date format will be the
+#' same as the input date format.
+#' 
 #' @param org.date original date
 #' @param delta amount of time to add to \code{org.date}
 #' @param date.format how to format the resulting date
 #' @param units the unit of time for which to take the difference. Defaults to "days".
 #' @param existing.missing.codes missing dates
 #' @param return.missing.code what to return if there is a missing input
+#' @return The new date after adding \code{delta} to \code{org.date}.
 #' @author Samuel Leung
 #' @export
+#' @examples
+#' addtoDate("2014/07/08", 10, date.format = "YYYY.MM.DD")
+#' addtoDate("2014/07/08", 10, date.format = "YYYY.MM.DD", units = "months")
+#' addtoDate("2014/07/08", -10, date.format = "YYYY.MM.DD", units = "years")
 addtoDate <- function(org.date, delta, date.format = "MM.DD.YYYY",
                       units = "days", existing.missing.codes = NA,
                       return.missing.code = NA) {
@@ -14,7 +27,7 @@ addtoDate <- function(org.date, delta, date.format = "MM.DD.YYYY",
     return(NA)
   if (length(unique(existing.missing.codes
                     [!is.na(existing.missing.codes)])) > 0 &
-      (d1 %in% existing.missing.codes | d2 %in% existing.missing.codes))
+      (org.date %in% existing.missing.codes | delta %in% existing.missing.codes))
     return(return.missing.code)
   delta <- as.numeric(delta)
   delta.in.days <- delta
