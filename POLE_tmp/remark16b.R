f25606553091d059cbb065722a8b49fe2a19c1b6
Derek Chiu <dchiu@bccrc.ca>
2015-07-16 14:46:37 -0700

    removed unnecessary files

--- POLE_tmp/remark16b.R
+++
@@ -1,7 +0,0 @@
-#dmat input data matrix 
-#dmat=dat1
-#event="OS"
-#event is the type of survival event we are studying labeled OS.yrs and OS.sts 
-
-# per email from Aline 2015-05-14 ... use Firth correction in interaction test
-# WARNING!!! coxphftest DOES NOT WORK WITH factor !!! need to change all factor to numeric!!!
