884539186d4925d23d42ede878b239035224c21d
Derek Chiu <dchiu@bccrc.ca>
2015-07-17 11:00:09 -0700

    make kmplot functions use a data argument

--- biostatUtil/R/doKMPlotsPOLEInitTrt.R
+++ biostatUtil/R/doKMPlotsPOLEInitTrt.R
@@ -1,15 +1,12 @@
 #' Make Kaplan-Meier plots with initial treatment
 #' @export
-doKMPlotsPOLEInitTrt <- function(surv.type, use.aline.plot = FALSE, ...) {
+doKMPlotsPOLEInitTrt <- function(data, surv.type,
+                                 use.aline.plot = FALSE, ...) {
   doKMPlots(
-    emdb[!emdb$POLE.mut.germline.as.missing%in%ALL.MISSING.CODES &
-           !emdb$init.treatment%in%ALL.MISSING.CODES,],
+    data[!data$POLE.mut.germline.as.missing %in% ALL.MISSING.CODES &
+           !data$init.treatment %in% ALL.MISSING.CODES, ],
     "POLE.x.init.treatment", "POLE mutation status x any adjuvant treatment",
-    line.color = c("black", "black", "red", "red"),
-    line.width = c(3, 1, 3, 1),
-    single.test.type = "none",
-    surv.type = surv.type, 
-    conf.int = TRUE,
-    use.aline.plot = use.aline.plot,
-    ...)
+    line.color = c("black", "black", "red", "red"), line.width = c(3, 1, 3, 1),
+    single.test.type = "none", surv.type = surv.type, 
+    conf.int = TRUE, use.aline.plot = use.aline.plot, ...)
 }
