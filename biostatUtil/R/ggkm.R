09646b0fedbeb9c4a9f27798703527b92d22fe33
Derek Chiu <dchiu@bccrc.ca>
2015-07-23 12:11:00 -0700

    the ... is actually needed

--- biostatUtil/R/ggkm.R
+++ biostatUtil/R/ggkm.R
@@ -35,7 +35,7 @@ ggkm <- function(sfit, sfit2 = NULL, table = TRUE, returns = FALSE,
                  xlims = c(0, max(sfit$time)), ylims = c(0, 1),
                  ystratalabs = NULL, ystrataname = NULL,
                  timeby = 5, pval = TRUE, HR = TRUE,
-                 use.firth = 1, subs = NULL, legend = FALSE) {
+                 use.firth = 1, subs = NULL, legend = FALSE, ...) {
   line.y.increment <- 0.05 # for annotate(), to indicate the much y should be incremented for each line
   
   # to satisfy R CMD check where variables can't be found when input data passed from ggplot
