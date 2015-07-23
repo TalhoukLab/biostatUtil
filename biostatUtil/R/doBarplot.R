d07d29ed4fea5db7c79a6b97fd6fdb7ca4160564
Derek Chiu <dchiu@bccrc.ca>
2015-07-23 11:29:28 -0700

    documentation for boxplot and barplot functions

--- biostatUtil/R/doBarplot.R
+++ biostatUtil/R/doBarplot.R
@@ -1,9 +1,18 @@
 #' Generates barplot
+#' 
+#' Generates barplot on biomarker variables
+#' 
+#' @param input.d input data frame
+#' @param data.description barplot title description
+#' @param biomarker.var.name variable name in \code{input.d} to graph on
+#' @param biomarker.name barplot x-axis label
+#' @param biomarker.value.names names of biomarker values
+#' @param digits number of digits to round to
+#' @return Barplot of biomarker frequencies
+#' @author Samuel Leung
+#' @note Function expects missing to be \code{NA}. Do not filter out missing data
+#' as this function reports missing data frequencies.
 #' @export
-##################################################################
-# function to generate barplot
-# NOTE: expect missing to be NA!!! DO NOT filter out missing biomarker data
-#       as this function reports missing data count
 doBarplot <- function(input.d, data.description, biomarker.var.name,
                       biomarker.name, biomarker.value.names, digits = 3) {
   biomarker <- input.d[, biomarker.var.name]
@@ -17,7 +26,7 @@ doBarplot <- function(input.d, data.description, biomarker.var.name,
           ylab = "Frequency", xlab = biomarker.name,
           main = paste0(data.description, "\n# scorable,missing: ",
                         sum(!is.na(biomarker)), "(",
-                        format(sum(!is.na(biomarker)) /nrow(input.d) * 100,
+                        format(sum(!is.na(biomarker)) / nrow(input.d) * 100,
                                digits = digits), "%)", ",",
                         sum(is.na(biomarker)), "(",
                         format(sum(is.na(biomarker)) / nrow(input.d) * 100,
--- biostatUtil/R/doBoxplotAmongSubtypes.R
+++ biostatUtil/R/doBoxplotAmongSubtypes.R
@@ -1,19 +1,26 @@
 #' Do a boxplot among subtypes
+#' 
+#' A stripchart (jitter plot) of a biomarker and some categorical subtype
+#' is superimposed on top of the boxplot
+#' @param input.d input data.frame
+#' @param data.description boxplot title description
+#' @param biomarker.var.name biomarker variable name in \code{input.d} to graph on
+#' @param biomarker.name boxplot y-axis label
+#' @param subtype.var.name subtype variable name in \code{input.d} to graph on
+#' @param subtype.name boxplot x-axis label
+#' @param pch stripchart plot style
+#' @param jitter amount of jitter in stripchart
+#' @param digits number of digits to round to
+#' @param ... additional arguments to \code{boxplot}
+#' @return a boxplot of biomarker values across different subtypes
+#' @note Expects subtype variable to be a factor. Also expects biomarker
+#' and subtype variable missing values to be \code{NA}
+#' @author Samuel Leung
 #' @export
-####################################################################
-# function to generate stripchart (jitter plot) of 
-# a biomarker x some categoriical subtype on top of a boxplot
-#
-# NOTE:
-# - expect subtype variable be factor
-# - expect biomarker and subtype variable missing to be NA
-#
-# example of pch ... 20=small dot, 16=bigger dot
-#
 doBoxplotAmongSubtypes <- function(input.d, data.description,
                                    biomarker.var.name, biomarker.name,
                                    subtype.var.name, subtype.name, pch = 4,
-                                   jitter = 0.1, digits = 2, boot.mean, ...) {
+                                   jitter = 0.1, digits = 2, ...) {
   temp.d <- input.d[(!is.na(input.d[, biomarker.var.name])) & 
                       (!is.na(input.d[, subtype.var.name])), ]
   biomarker <- temp.d[, biomarker.var.name]
@@ -22,7 +29,7 @@ doBoxplotAmongSubtypes <- function(input.d, data.description,
   } else {
     subtype <- temp.d[, subtype.var.name]
   }
-  xbar <- tapply(biomarker, subtype, boot.mean)
+  xbar <- tapply(biomarker, subtype, bootMean)
   test.name <- "Kruskal-Wallis"
   p.value <- kruskal.test(biomarker ~ subtype)$p.value
   if (length(names(table(subtype))) == 2) {
