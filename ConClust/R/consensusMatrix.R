cfbb74b89bdcc71f41ed732f014fe53090b40791
Derek Chiu <dchiu@bccrc.ca>
2015-09-02 11:35:51 -0700

    optional vector of weights for consensusMatrix function

--- ConClust/R/consensusMatrix.R
+++ ConClust/R/consensusMatrix.R
@@ -1,19 +1,27 @@
 #' Consensus matrix
 #'
-#' Returns the consensus matrix given a data matrix
+#' Returns the (weighted) consensus matrix given a data matrix
 #'
-#' Input matrix has rows as samples, columns as replicates.
+#' Input matrix has rows as samples, columns as replicates. If a meta-consensus matrix
+#' is desired, where consensus classes of different clustering algorithms are aggregated,
+#' we can construct a weighted meta-consensus matrix using \code{weights}.
+#'
 #' @param dat data matrix
+#' @param weights a vector of weights for each algorithm used in meta-consensus
+#' clustering. Must have \code{length(weights)} equal to \code{ncol(dat)}.
 #' @return a consensus matrix
 #' @author Derek Chiu
 #' @export
-consensusMatrix <- function(dat) {
-  all.CM <- plyr::alply(dat, 2, connectivityMatrix)
+consensusMatrix <- function(dat, weights = NULL) {
   all.IM <- plyr::alply(dat, 2, indicatorMatrix)
-
-  sum.CM <- Reduce('+', all.CM)
+  all.CM <- plyr::alply(dat, 2, connectivityMatrix)
   sum.IM <- Reduce('+', all.IM)
-
+  if (!is.null(weights)) {
+    weighted.CM <- mapply('*', all.CM, weights, SIMPLIFY = FALSE)
+    sum.CM <- Reduce('+', weighted.CM) * length(weights)
+  } else {
+    sum.CM <- Reduce('+', all.CM)
+  }
   cons.mat <- Reduce('/', list(sum.CM, sum.IM))
   return(cons.mat)
 }
