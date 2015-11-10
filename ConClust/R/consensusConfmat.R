1010153f6d39545fe744577b9905e043d33f7d10
Derek Chiu <dchiu@bccrc.ca>
2015-11-09 16:35:20 -0800

    added minFnorm function to fix construction of confusion matrix

--- ConClust/R/consensusConfmat.R
+++ ConClust/R/consensusConfmat.R
@@ -1,24 +1,24 @@
 #' Confusion matrix between consensus clusters and true classes
-#' 
-#' Combines the cluster assignments from multiple algorithms, then 
+#'
+#' Combines the cluster assignments from multiple algorithms, then
 #' compares to the true class partitions.
-#' 
+#'
 #' @param clust vector of cluster assignments
 #' @param cl.true true class labels
 #' @param k number of clusters
 #' @param pred.lab label for the predicted clusters
 #' @param ref.lab label for the reference classes
-#' @return A confusion matrix with the predicted cluster 
+#' @return A confusion matrix with the predicted cluster
 #' assignments compared to the reference true class labels.
-#' @author Derek Chiu 
+#' @author Derek Chiu
 #' @importFrom magrittr set_names set_rownames
 #' @export
 consensusConfmat <- function(clust, cl.true, k,
                              pred.lab = "Prediction", ref.lab = "Reference") {
   . <- NULL
   table(clust, cl.true, dnn = c(eval(pred.lab), eval(ref.lab))) %>%
-    extract(names(sort(apply(., 1, which.max))), ) %>%
+    minFnorm() %>%
     set_rownames(colnames(.)) %>%
-    as.table() %>% 
+    as.table() %>%
     return()
 }
