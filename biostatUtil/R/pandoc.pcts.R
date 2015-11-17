ee6733bdff1bac95e9c11975552fd7f541bb723e
Derek Chiu <dchiu@bccrc.ca>
2015-11-17 11:42:55 -0800

    modify pandoc.pcts so that it can take in row, col, or both pcts

--- biostatUtil/R/pandoc.pcts.R
+++ biostatUtil/R/pandoc.pcts.R
@@ -1,5 +1,8 @@
 pandoc.pcts <- function(char) {
   count <- as.integer(char[1])
-  pcts <- as.numeric(char[2:3]) * 100
-  return(paste0(count, " (", pcts[1], "%, ", pcts[2], "%)"))
+  pcts <- as.numeric(char[-1]) * 100
+  if (length(char) > 2)
+    return(paste0(count, " (", pcts[1], "%, ", pcts[2], "%)"))
+  else
+    return(paste0(count, " (", pcts, "%)"))
 }
