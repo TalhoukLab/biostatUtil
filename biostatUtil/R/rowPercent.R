437f4aa40218d7f3e575459d9a1e3a90a3f925b6
Derek Chiu <dchiu@bccrc.ca>
2015-11-17 11:42:27 -0800

    fixed rowPercent function

--- biostatUtil/R/rowPercent.R
+++ biostatUtil/R/rowPercent.R
@@ -21,7 +21,7 @@
 #' rowPercent(A, pretty.text = TRUE)
 #' rowPercent(A, pretty.text = TRUE, keep = TRUE)
 rowPercent <- function(t, pretty.text = FALSE, keep = TRUE, digits = 4) {
-  if (is.matrix(t)) {
+  if (!is.null(dim(t))) {
     pcts <- t / apply(t, 1, sum)
     if (pretty.text) {
       pcts <- apply(pcts * 100, c(1, 2),
