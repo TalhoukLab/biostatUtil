6013d26a898fce487a793d8bcfcb2bf657c5e4cc
Aline Talhouk <atalhouk@bccrc.ca>
2016-01-04 10:33:25 -0800

    new code updated

--- NewCodeLab/exploreData.R
+++ NewCodeLab/exploreData.R
@@ -1,4 +1,4 @@
-library(dplyr)
+
 types <- unname(sapply(emdb.all, class))
 fd <- emdb.all[,types%in%c("factor", "numeric")]
 type.fd <- unname(sapply(fd, class))
@@ -10,13 +10,15 @@ catvars <- colnames(fd)[fac.ind]
 for (i in 1: length(catvars)){
 print(i)
 x= fd[, catvars[i]]
+tx=table(x, useNA = "ifany")
 par(mfrow=c(2,1),mar=c(3.1,9.5,4.1,2.1))
 wr.lap <- wrap.labels(names(table(x,useNA = "ifany")), 35)
-barplot(prop.table(table(x, useNA = "ifany"))*100,
+barplot(prop.table(tx)*100,
         horiz = T, las=2,names.arg = wr.lap, offset = 0,
-        main=catvars[i], xlab = "%", cex.names = 0.5)
-mat <- data.matrix(table(x,useNA = "ifany"));colnames(mat) <- "Freq"
-gplots::textplot(mat)
+        main=catvars[i], xlab = "%", cex.names = 0.5, col="lightblue")
+mat <- data.matrix(cbind(tx,round(prop.table(tx)*100,1)));colnames(mat) <- c("Freq","%")
+
+PerformanceAnalytics::textplot(mat,wrap=F)
 }
 
 
