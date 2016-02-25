7e1f737d5b200df4186f75b07d309d8970fc0545
Aline Talhouk <atalhouk@bccrc.ca>
2016-02-25 10:18:38 -0800

    delete files

--- nanonstringr.Rcheck/00_pkg_src/nanonstringr/R/nanostringr-package.r
+++
@@ -1,8 +0,0 @@
-#' nanostringr: Quality Control and normalization for nanostringr platform
-#'
-#' The nanostringr package provides a novel set of tools used for quality control purposes
-#' in the NanoString platform. It includes batch adjustment using the ratio method thus far.
-#'
-#' @name nanostringr
-#' @docType package
-NULL
