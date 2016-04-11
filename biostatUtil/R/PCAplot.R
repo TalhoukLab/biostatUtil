1970dddd787b819a8bc2573e424a41a878e246f9
Aline Talhouk <atalhouk@bccrc.ca>
2016-04-11 08:32:13 -0700

    PCAplot fixed and Variance explained added to axes

--- biostatUtil/R/PCAplot.R
+++ biostatUtil/R/PCAplot.R
@@ -1,21 +1,33 @@
 #' function to plot the first three PCAs and draw ellipses highlighting differences 
 #' in the levels of a factor
 
-# @export
+#' @export
 PCAplot<-function(by,tdat, mainT = "", sideT = ""){
   require(ggplot2)
-  pcaX <- prcomp(tdat,rtx=T,scale.=T)$x
-  
+  p <- prcomp(tdat,rtx=T,scale.=T)
+  var3 <- round(((p$sdev^2 / sum(p$sdev^2))*100)[1:3],2)
+
+  pcaX <- p$x
   scores<-pcaX[,1:3]
   df <- data.frame(var.name=factor(by),scores)
   
   pc1.2 <- qplot(x=PC1, y=PC2, data=df, colour=var.name)+
     stat_ellipse(geom = "polygon", level=0.9, alpha=0.1,size=0.05, aes(fill = var.name))+
-    theme_bw()+theme(legend.title=element_blank())
+    theme_bw()+theme(legend.title=element_blank())+
+    xlab(paste0("PC1 (",var3[1],"% Var)"))+
+    ylab(paste0("PC2 (",var3[2],"%Var)"))
   
-  pc1.3 <- qplot(x=PC1, y=PC3, data=df, colour=var.name) + stat_ellipse(geom = "polygon", level=0.9, alpha=0.1,size=0.05, aes(fill = var.name))+theme_bw()+ theme(legend.title=element_blank())
+  pc1.3 <- qplot(x=PC1, y=PC3, data=df, colour=var.name)+ 
+    stat_ellipse(geom = "polygon", level=0.9, alpha=0.1,size=0.05, aes(fill = var.name))+
+    theme_bw()+ theme(legend.title=element_blank())+ 
+    xlab(paste0("PC1 (",var3[1],"% Var)"))+ 
+    ylab(paste0("PC3 (",var3[3],"%Var)"))
   
-  pc2.3 <- qplot(data=df,x=PC2, y=PC3,colour=var.name) + stat_ellipse(geom = "polygon", level=0.9, alpha=0.1,size=0.05, aes(fill = var.name))+theme_bw()+ theme(legend.title=element_blank())
+  pc2.3 <- qplot(data=df,x=PC2, y=PC3,colour=var.name) + 
+    stat_ellipse(geom = "polygon", level=0.9, alpha=0.1,size=0.05, aes(fill = var.name))+ 
+    theme_bw()+ theme(legend.title=element_blank())+
+    xlab(paste0("PC2 (",var3[3],"% Var)"))+
+    ylab(paste0("PC3 (",var3[3],"%Var)"))
   
   multiplot(pc1.2, pc1.3, pc2.3, cols=1)
 }
