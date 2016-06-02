861505da231ecafb28e8db457c700ad9ce629784
samuelc <samuelc@samuelc-win10>
2016-06-02 14:37:12 -0700

    plotSchoenfeld
    plot Schoenfeld residual plot

---
+++ biostatUtil/R/plotSchoenfeld.R
@@ -0,0 +1,47 @@
+#' Do Schoenfeld residual plot
+#' 
+#' @param input.d input \code{data.frame}
+#' @param input.formula survival formula to \code{Surv}
+#' @param vars.to.plot variable(s) to plot
+#' @param main title for each variable (i.e. main can be an array)
+#' @author Samuel Leung
+#' @export
+#' @examples 
+#' # Base output
+#' library(biostatUtil)
+#' test1 <- list(time=c(4,3,1,1,2,2,3), 
+#' status=c(1,1,1,0,1,1,0), 
+#' x=c(0,2,1,1,1,0,0), 
+#' sex=c(0,0,0,0,1,1,1))  
+#' 
+#' # Pretty output
+#' plotSchoenfeld(test1, Surv(time, status) ~ x + strata(sex), "x")
+plotSchoenfeld <- function(input.d, input.formula, vars.to.plot=NULL, main=NULL) {
+	pos <- 1
+	assign(".my.data", input.d, envir = as.environment(pos))
+	# do cox model first
+	fit <- survival::coxph(input.formula,.my.data)
+	
+	var.names <- all.vars(input.formula[[3]]) # there may ne > 1 terms in formula
+	for (var.name in var.names) {
+		names(fit$coefficients) <- sapply(names(fit$coefficients),function(x){
+					return(ifelse(var.name==x,x,sub(var.name,"",x)))
+				})
+	}
+	cat("1")
+	fit.zph <- survival::cox.zph(fit)
+	cat("2")
+	# determine which variable to plot
+	if (is.null(vars.to.plot)) {
+		plot(fit.zph,main=ifelse(is.null(main),"",main)) # plot all
+	} else {
+		for (i in 1:length(vars.to.plot)) {
+			plot(
+				fit.zph,
+				var=which(names(fit$coefficients)==vars.to.plot[i]),
+				main=ifelse(is.null(main),"",main[min(length(main),i)])
+			)
+		}
+	}
+	return(list("fit"=fit,"fit.zph"=fit.zph))
+}
