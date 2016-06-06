9bc2ae60741d24f006f09a7744d75a7bf0be53fb
Derek Chiu <dchiu@bccrc.ca>
2016-06-06 10:09:51 -0700

    doc for plotSchoenfeld

--- biostatUtil/R/plotSchoenfeld.R
+++ biostatUtil/R/plotSchoenfeld.R
@@ -8,7 +8,6 @@
 #' @export
 #' @examples 
 #' # Base output
-#' library(biostatUtil)
 #' test1 <- list(time=c(4,3,1,1,2,2,3), 
 #' status=c(1,1,1,0,1,1,0), 
 #' x=c(0,2,1,1,1,0,0), 
@@ -16,32 +15,34 @@
 #' 
 #' # Pretty output
 #' plotSchoenfeld(test1, Surv(time, status) ~ x + strata(sex), "x")
-plotSchoenfeld <- function(input.d, input.formula, vars.to.plot=NULL, main=NULL) {
+plotSchoenfeld <- function(input.d, input.formula, vars.to.plot = NULL,
+                           main = NULL) {
 	pos <- 1
-	assign(".my.data", input.d, envir = as.environment(pos))
+	.my.data <- input.d
+	assign(".my.data", .my.data, envir = as.environment(pos))
 	# do cox model first
-	fit <- survival::coxph(input.formula,.my.data)
+	fit <- survival::coxph(input.formula, .my.data)
 	
 	var.names <- all.vars(input.formula[[3]]) # there may ne > 1 terms in formula
 	for (var.name in var.names) {
-		names(fit$coefficients) <- sapply(names(fit$coefficients),function(x){
-					return(ifelse(var.name==x,x,sub(var.name,"",x)))
-				})
+	  names(fit$coefficients) <- sapply(names(fit$coefficients), function(x) {
+	    return(ifelse(var.name == x, x, sub(var.name, "", x)))
+	  })
 	}
 	cat("1")
 	fit.zph <- survival::cox.zph(fit)
 	cat("2")
 	# determine which variable to plot
 	if (is.null(vars.to.plot)) {
-		plot(fit.zph,main=ifelse(is.null(main),"",main)) # plot all
+	  plot(fit.zph, main = ifelse(is.null(main), "", main)) # plot all
 	} else {
-		for (i in 1:length(vars.to.plot)) {
-			plot(
-				fit.zph,
-				var=which(names(fit$coefficients)==vars.to.plot[i]),
-				main=ifelse(is.null(main),"",main[min(length(main),i)])
-			)
-		}
+	  for (i in 1:length(vars.to.plot)) {
+	    plot(
+	      fit.zph,
+	      var = which(names(fit$coefficients) == vars.to.plot[i]),
+	      main = ifelse(is.null(main), "", main[min(length(main), i)])
+	    )
+	  }
 	}
-	return(list("fit"=fit,"fit.zph"=fit.zph))
+	return(list("fit" = fit, "fit.zph" = fit.zph))
 }
