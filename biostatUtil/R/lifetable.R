d384f14e086c7c0e36b97842058be92099eda2b9
Derek Chiu <dchiu@bccrc.ca>
2016-06-15 16:21:47 -0700

    options to round and turn off var names in lifetable

--- biostatUtil/R/lifetable.R
+++ biostatUtil/R/lifetable.R
@@ -10,6 +10,9 @@
 #' @param times A vector of endpoints of time intervals to show life table calculations.
 #' By default, these are \code{ntimes} evenly spaced out endpoints based on the full range
 #' of survival times.
+#' @param nround number of digits to round table values
+#' @param var.name logical; if \code{TRUE} (default), the variable name is appended to the
+#' beginning of each stratum in the lifetable's \code{strata} column
 #'
 #' @return A table with the following columns:
 #' \item{strata}{name of specific group in variable}
@@ -32,10 +35,11 @@
 #' @examples
 #' obj <- survfit(Surv(futime, fustat) ~ rx, data = ovarian)
 #' lifetable(obj)
-#' lifetable(obj, ntimes = 4)
+#' lifetable(obj, ntimes = 4, var.name = FALSE)
 #' lifetable(obj, ntimes = 4, times = c(200, 500, 800, 1000))
 lifetable <- function(obj, ntimes = 3,
-                      times = round(quantile(obj$time, 1/ntimes * rep(1:ntimes)))) {
+                      times = round(quantile(obj$time, 1/ntimes * rep(1:ntimes))),
+                      nround = 3, var.name = TRUE) {
   . <- NULL
   cuts <- cumsum(obj$strata)
   ind <- lapply(splitAt(obj$time, cuts), function(x)
@@ -44,9 +48,14 @@ lifetable <- function(obj, ntimes = 3,
                splitAt(obj$n.censor, cuts), ind, SIMPLIFY = FALSE)
   es <- mapply(function(x, ind) sapply(splitAt(x, ind), sum)[-(length(ind) + 1)],
                splitAt(obj$n.event, cuts), ind, SIMPLIFY = FALSE)
+  if (var.name)
+    strata <- names(obj$strata)
+  else
+    strata <- gsub(".+=", "\\1", names(obj$strata))
   tab <- mapply(function(n, c, e) KMsurv::lifetab(c(0, times), n, c, e),
                 obj$n, cs, es, SIMPLIFY = FALSE) %>% 
+    lapply(round, nround) %>% 
     plyr::ldply(function(.) cbind(times = rownames(.), .)) %>% 
-    cbind(strata = rep(names(obj$strata), each = ntimes), .)
+    cbind(strata = rep(strata, each = ntimes), .)
   return(tab)
 }
