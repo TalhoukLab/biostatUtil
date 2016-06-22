cedef0667a58e8382d72adb41221d513fc2f415b
Derek Chiu <dchiu@bccrc.ca>
2016-06-22 13:02:34 -0700

    give option to show only a summarized lifetable

--- biostatUtil/R/lifetable.R
+++ biostatUtil/R/lifetable.R
@@ -11,9 +11,11 @@
 #' By default, these are \code{ntimes} evenly spaced out endpoints based on the full range
 #' of survival times.
 #' @param nround number of digits to round table values
-#' @param var.name logical; if \code{TRUE} (default), the variable name is appended to the
+#' @param show.strata logical; if \code{TRUE} (default), the variable name is appended to the
 #' beginning of each stratum in the lifetable's \code{strata} column
-#'
+#' @param strata.name column name for the different strata
+#' @param summary logical; if \code{TRUE}, a case processing summary is shown with number 
+#' of subjects, events, censored, and percent censored per stratum.
 #' @return A table with the following columns:
 #' \item{strata}{name of specific group in variable}
 #' \item{times}{time interval}
@@ -35,12 +37,13 @@
 #' @examples
 #' obj <- survfit(Surv(futime, fustat) ~ rx, data = ovarian)
 #' lifetable(obj)
-#' lifetable(obj, ntimes = 4, var.name = FALSE)
+#' lifetable(obj, ntimes = 4, show.strata = FALSE)
 #' lifetable(obj, ntimes = 4, times = c(200, 500, 800, 1000))
 lifetable <- function(obj, ntimes = 3,
                       times = round(quantile(obj$time, 1/ntimes * rep(1:ntimes))),
-                      nround = 3, var.name = TRUE) {
-  . <- NULL
+                      nround = 3, show.strata = TRUE, strata.name = "strata",
+                      summary = FALSE) {
+  . <- nevent <- nlost <- nsubs <- plost <- NULL
   cuts <- cumsum(obj$strata)
   if (ntimes > 1) {
     ind <- sapply(splitAt(obj$time, cuts), function(x) {
@@ -57,7 +60,7 @@ lifetable <- function(obj, ntimes = 3,
                splitAt(obj$n.censor, cuts), ind, SIMPLIFY = FALSE)
   es <- mapply(function(x, ind) sapply(splitAt(x, ind), sum)[-(length(ind) + 1)],
                splitAt(obj$n.event, cuts), ind, SIMPLIFY = FALSE)
-  if (var.name)
+  if (show.strata)
     strata <- names(obj$strata)
   else
     strata <- gsub(".+=", "\\1", names(obj$strata))
@@ -66,5 +69,16 @@ lifetable <- function(obj, ntimes = 3,
     lapply(round, nround) %>% 
     plyr::ldply(function(.) cbind(times = rownames(.), .)) %>% 
     cbind(strata = rep(strata, each = ntimes), .)
+  if (summary) {
+    tab <- tab %>% 
+      select(strata, nsubs, nevent, nlost) %>%
+      mutate(strata = as.character(strata)) %>%
+      rbind(c("Overall", colSums(.[-1]))) %>%
+      mutate_each_(funs(as.numeric), names(.)[-1]) %>%
+      mutate(plost = paste0(sprintf("%.1f", nlost / nsubs * 100), "%")) %>%
+      dplyr::rename(`Total N` = nsubs, `N of Events` = nevent,
+                    `N of Censored` = nlost, `Percent Censored` = plost)
+  }
+  colnames(tab)[1] <- eval(strata.name)
   return(tab)
 }
