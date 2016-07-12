8f5d280b7038f266b4b70eec3a2edbf50a2cded2
Derek Chiu <dchiu@bccrc.ca>
2016-07-12 13:35:45 -0700

    generate docs for ratioWithMissing and ggkm

--- biostatUtil/R/ratioWithMissing.R
+++ biostatUtil/R/ratioWithMissing.R
@@ -2,7 +2,8 @@
 #' 
 #' Returns the ratio of input vectors: x/y with the ability to specify
 #' which values are missing
-#' @param x input vector,  y input vector
+#' @param x input vector
+#' @param y input vector
 #' @param missing.value missing values in \code{x}
 #' @param return.missing.value character to return for missing values
 #' @note \code{NAs} are ignored.
@@ -11,15 +12,15 @@
 #' @examples 
 #' x <- c(10:1)
 #' y <- c(1:10)
-#' maxWithMissing(x,y,c(1:2)
+#' ratioWithMissing(x, y, c(1:2))
 #' 
 #' ## All missing
-#' maxWithMissing(x,y,c(1:10))
-#' maxWithMissing(x,y,c(1:10), return.missing.value = "all missing")
+#' ratioWithMissing(x, y, c(1:10))
+#' ratioWithMissing(x, y, c(1:10), return.missing.value = "all missing")
 ratioWithMissing <- function(x, y, missing.value = -1, return.missing.value = -1) {
 	x[x %in% missing.value] <- NA
 	y[y %in% missing.value] <- NA
-	ratio <- as.numeric(x)/as.numeric(y)
+	ratio <- as.numeric(x) / as.numeric(y)
 	ratio[is.na(ratio)] <- return.missing.value
 	return(ratio)
 }
