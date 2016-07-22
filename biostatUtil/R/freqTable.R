e70be41cb60553113b5f1b587144c7ab0e930788
Derek Chiu <dchiu@bccrc.ca>
2016-07-22 11:44:13 -0700

    freqTable: check description length, streamline no-missing case, fix factor errors

--- biostatUtil/R/freqTable.R
+++ biostatUtil/R/freqTable.R
@@ -12,15 +12,18 @@
 #' @param plot logical; if \code{TRUE}, a barplot is produced.
 #'
 #' @return A data frame with the following columns
-#' \item{Class}{Tells you which scores are valid and which are missing}
+#' \item{Class}{Tells you which scores are valid and which are missing. Not shown if
+#' there are no missing values.}
 #' \item{Score}{Different levels}
 #' \item{Frequency}{Count for each score}
 #' \item{Percent}{Percent of Frequency out of the grand total}
-#' \item{Valid Percent}{Percent of Frequency out of the Valid scores}
+#' \item{Valid Percent}{Percent of Frequency out of the Valid scores. Not applicable if
+#' there are no missing values.}
 #' \item{Cumulative Percent}{Accumulated Percent of Frequency out of the Valid Scores}
 #' \item{Description}{If \code{description} is given, a description for each level}
 #' 
 #' @author Derek Chiu
+#' @seealso \code{\link{freq}}
 #' @export
 #' 
 #' @examples 
@@ -33,33 +36,38 @@
 #' 
 #' # Treat vowels as missing
 #' freqTable(x, missing = c("a", "e", "i", "o", "u"), round = 2)
-freqTable <- function(x, levels = sort(unique(x)), missing = NULL, description = NULL,
+freqTable <- function(x, levels = sort(unique(as.character(x))),
+                      missing = NULL, description = NULL,
                       round = 1, plot = FALSE) {
   . <- Class <- Frequency <- Score <- `Valid Percent` <- NULL
+  
   tab <- descr::freq(x, user.missing = missing, plot = plot) %>% 
     as.data.frame() %>% 
-    rbind(c(sum(.$Frequency[!is.na(.$`Valid Percent`) & rownames(.) != "Total"]),
-            sum(.$Percent[!is.na(.$`Valid Percent`) & rownames(.) != "Total"]), 100)) %>% 
-    cbind(Score = factor(rownames(.), c(levels, "Total", "")), .) %>% 
+    cbind(Score = factor(rownames(.), c(levels, "Total")), .) %>% 
     cbind(Class = factor(ifelse(is.na(.$`Valid Percent`), "Missing",
                                 ifelse(grepl("Total", .$Score), "Total", "Valid")),
                          c("Valid", "Missing", "Total")), .) %>% 
+    rbind(c("Valid", "Total",
+            sum(.$Frequency[!is.na(.$`Valid Percent`) & .$Class == "Valid"]),
+            sum(.$Percent[!is.na(.$`Valid Percent`) & .$Class == "Valid"]), 100)) %>%
     arrange(Class, Score) %>% 
+    mutate_each(funs(as.numeric), matches("Frequency|Percent")) %>% 
     mutate(Score = ifelse(Class == "Total" & Score == "Total", "",
                           ifelse(is.na(Score), "Total", as.character(Score))),
            `Valid Percent` = ifelse(Class == "Total", NA, `Valid Percent`),
            `Cumulative Percent` = ifelse(!is.na(`Valid Percent`) & Score != "Total",
                                          cumsum(Frequency) / max(Frequency[Class == "Valid"]) * 100, NA),
            Class = ifelse(duplicated(Class), "", as.character(Class))) %>% 
-    mutate_each(funs(ifelse(sprintf(paste0("%.", round, "f"), .) == "NA", "",
-                            sprintf(paste0("%.", round, "f"), .))),
-                grep("Percent", names(.)))
-  if (is.null(missing))
+    mutate_each(funs(round(., 1)), contains("Percent"))
+  if (is.null(missing)) {
     tab <- tab %>% 
-      extract(-which(.$Score == "Total"), ) %>% 
-      select(-`Valid Percent`)
-  if (!is.null(description))
+      extract(-which(.$Class == "Total"), ) %>% 
+      select(-matches("Class|Valid Percent"))
+  }
+  if (!is.null(description)) {
+    assertthat::assert_that(length(levels) == length(description))
     tab <- tab %>%
-      mutate(Description = c(append(description, "", which(Score == "Total") - 1), ""))
+    mutate(Description = c(append(description, "", which(Score == "Total") - 1), ""))
+  }
   return(tab)
 }
