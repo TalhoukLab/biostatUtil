6756347f1fe5432cf8810543fe895b82a1e9f7cd
Derek Chiu <dchiu@bccrc.ca>
2016-08-08 16:00:04 -0700

    combine docs for startsWith & endsWith

--- biostatUtil/R/startsWith.R
+++ biostatUtil/R/startsWith.R
@@ -1,29 +1,43 @@
-#' Check if a string starts with a partciular string
+#' Check if a string starts or ends with another string
 #' 
-#' Returns a logical indicating whether the string in the first argument starts
-#' with the string in the second argument.
+#' Returns a logical indicating whether the string in the first argument starts 
+#' or ends with the string in the second argument.
 #' 
-#' Although intended for strings, \code{startsWith} works with numeric vectors
-#' as well.
+#' Although intended for strings, the function works with numeric vectors as
+#' well.
 #' 
 #' @param a string to be checked against
-#' @param b string ending to be checked
-#' @return logical; if TRUE, \code{a} is a string that starts with \code{b}.
-#' @author Samuel Leung
-#' @seealso \code{\link{endsWith}}
+#' @param b prefix/suffix to check
+#' @return logical; if TRUE, \code{a} is a string that starts or ends with
+#'   \code{b}.
+#' @author Samuel Leung, Derek Chiu
 #' @export
 #' @examples 
-#' ## Strings
+#' # Strings
 #' startsWith("pineapple", "pine")
 #' startsWith("apple", "app")
 #' startsWith("apple", "ppl")
+#' endsWith("pineapple", "apple")
+#' endsWith("apple", "app")
 #' 
-#' ## Numerics
+#' # Numerics
 #' startsWith(123.4, 123)
 #' startsWith(45, 5)
+#' endsWith(123.4, 3.4)
+#' endsWith(45, 4)
 startsWith <- function(a, b) {
-  if (length(grep(b, a)) == 0)
+  if (!grepl(b, a))
     return(FALSE)
   else
     return(substr(a, 1, nchar(b)) == b)
+}
+
+#' @inheritParams startsWith
+#' @rdname startsWith
+#' @export
+endsWith <- function(a, b) {
+  if (!grepl(b, a))
+    return(FALSE)
+  else
+    return(substr(a, nchar(a) - nchar(b) + 1, nchar(a)) == b)
 }
