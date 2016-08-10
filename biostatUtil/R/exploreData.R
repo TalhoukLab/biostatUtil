5c84d9f6f0849ab7c7a99241f0fda1407cfa64cd
Derek Chiu <dchiu@bccrc.ca>
2016-08-10 15:04:51 -0700

    fix exploreData

--- biostatUtil/R/exploreData.R
+++ biostatUtil/R/exploreData.R
@@ -6,33 +6,35 @@
 #'   be analysed)
 #' @author Aline Talhouk
 #' @export
-exploreData <- function(datmat){
+exploreData <- function(datmat) {
   types <- unname(sapply(datmat, class))
   fd <- datmat[, types %in% c("factor", "numeric")]
   type.fd <- unname(sapply(fd, class))
   num.ind <- type.fd %in% c("numeric")
   fac.ind <- type.fd %in% c("factor")
+  numvars <- colnames(fd)[num.ind]
   catvars <- colnames(fd)[fac.ind]
   pdf("DataSummary.pdf")
-  
-  for (i in 1:length(catvars)) {
-    x = fd[, catvars[i]]
-    tx = table(x, useNA = "ifany")
-    par(mfrow = c(2, 1), mar = c(3.1, 9.5, 4.1, 2.1))
-    biostatUtil::barplotSum(tx, catvars[i])
-    mat <- data.matrix(cbind(tx, round(prop.table(tx) * 100, 1)))
-    colnames(mat) <- c("Freq", "%")
-    rownames(mat)[is.na(rownames(mat))] <- "Missing"
-    tmat <- rbind(mat, apply(mat[, 1:2], 2, sum))
-    PerformanceAnalytics::textplot(tmat, wrap = FALSE)
+  if (length(catvars) > 0) {
+    for (i in 1:length(catvars)) {
+      x = fd[, catvars[i]]
+      tx = table(x, useNA = "ifany")
+      par(mfrow = c(2, 1), mar = c(3.1, 9.5, 4.1, 2.1))
+      barplotSum(tx, catvars[i])
+      mat <- data.matrix(cbind(tx, round(prop.table(tx) * 100, 1)))
+      colnames(mat) <- c("Freq", "%")
+      rownames(mat)[is.na(rownames(mat))] <- "Missing"
+      tmat <- rbind(mat, apply(mat[, 1:2], 2, sum))
+      PerformanceAnalytics::textplot(tmat, wrap = FALSE)
+    }
   }
-  
-  numvars <- colnames(fd)[num.ind]
-  for (i in 1:length(numvars)) {
-    par(mfrow = c(2, 1))
-    x = fd[, numvars[i]]
-    biostatUtil::boxplotSum(x, numvars[i])
-    biostatUtil::histSum(x)
+  if (length(numvars) > 0) {
+    for (i in 1:length(numvars)) {
+      par(mfrow = c(2, 1))
+      x = fd[, numvars[i]]
+      boxplotSum(x, numvars[i])
+      histSum(x)
+    }
   }
   dev.off()
 }
