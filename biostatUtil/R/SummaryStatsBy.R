45267e90d21fe21224c7b322d805f2fb3698e434
Derek Chiu <dchiu@bccrc.ca>
2016-08-16 16:21:22 -0700

    shorten var names, make column names once, various speed improvements

--- biostatUtil/R/SummaryStatsBy.R
+++ biostatUtil/R/SummaryStatsBy.R
@@ -1,18 +1,31 @@
-#' Generate cohort characteristics
+#' Generate summary statistics
 #' 
-#' Splits the data by two variables, computing relevant statistics for each variable.
+#' Splits the data by two variables, computing relevant statistics for each 
+#' variable.
+#' 
+#' The \code{data} is split by two variables, \code{by1} and \code{by2}, and 
+#' statistics are computed for continuous variables. Statistics currently 
+#' supported include \code{ mean, sd, median, IQR, range}, and the number of 
+#' missing cases. For factor variables, the counts, column and row percentages 
+#' are shown for each of the variable levels.
+#' 
+#' Note that marginal statistics are also shown for \code{by1}, so the order in 
+#' which you split \code{data} matters.
+#' 
+#' There are four print options for the output: \code{raw} gives the output as a
+#' character matrix, \code{pandoc} gives a Pandoc-friendly output for Word and 
+#' PDF reports, \code{html} gives HTML supported output, and \code{long} is a 
+#' tidy version of \code{raw}.
 #' 
 #' @param data The \code{data.frame} containing the data
-#' @param by1 split variable 1
-#' @param by2 split variable 2
-#' @param var.names The variables that you want the statistics for
-#' @param stats statistics to show compute for continuous variables
-#' @param marker.description The description for the variable(s) to split
-#' @param groups.description The groupings of the marker
-#' @param var.descriptions descriptions of \code{var.names}
+#' @param by1 character string of splitting variable 1
+#' @param by2 character string of splitting variable 2
+#' @param var.names character vector of variables to compute statistics for
+#' @param stats statistics to compute for continuous variables
 #' @param digits number of digits to round to
-#' @param format format to return the table in. Either "raw", "pandoc" (for Word and PDF),
-#' "html", or "long" format for graphing and data manipulation using raw values.
+#' @param format format to return the table in. Either "raw", "pandoc" (for Word
+#'   and PDF), "html", or "long" format for graphing and data manipulation using
+#'   raw values.
 #' @author Aline Talhouk, Derek Chiu
 #' @export
 #' @examples 
@@ -22,13 +35,10 @@
 #' SummaryStatsBy(mtcars, by1 = "cyl", by2 = "gear", var.names = c("mpg"))
 SummaryStatsBy <- function(data, by1, by2, var.names,
                            stats = c("mean", "sd", "median", "IQR", "range",
-                                     "missing"),
-                           marker.description = by1, groups.description = by2,
-                           var.descriptions = var.names, digits = 3,
+                                     "missing"), digits = 3,
                            format = c("raw", "pandoc", "html", "long")) {
   . <- stat <- value <- grp <- vars <- lev <- NULL
   var.dat <- data[, var.names, drop = FALSE]
-  facets <- data[, c(by1, by2)]
   types <- sapply(var.dat, class)
   num.ind <- types %in% c("numeric", "integer")
   fac.ind <- types %in% c("factor", "character")
@@ -37,89 +47,96 @@ SummaryStatsBy <- function(data, by1, by2, var.names,
   }
   if (sum(num.ind) > 0) {
     num.var <- var.names[num.ind]
-    num.dat <- structure(data.frame(var.dat[num.var], facets),
+    num.dat <- structure(data.frame(var.dat[num.var], data[, c(by1, by2)]),
                          names = c(num.var, by1, by2))
   } else {
     num.var <- num.dat <- NULL
   }
   if (sum(fac.ind) > 0) {
     fac.var <- var.names[fac.ind]
-    fac.dat <- structure(data.frame(var.dat[fac.var], facets),
+    fac.dat <- structure(data.frame(var.dat[fac.var], data[, c(by1, by2)]),
                          names = c(fac.var, by1, by2))
   } else {
     fac.var <- fac.dat <- NULL
   }
-  
-  row.order <- unlist(lapply(num.var, function(x)
-    paste0(x, c("", paste0(".", stats)))))
-  if (all(c("mean", "sd") %in% stats)) {
-    stats <- stats[-grep("sd", stats)]
-    row.order <- row.order[-grep("sd", row.order)]
-  }
+  col.names <- unique(data[, c(by1, by2)]) %>% 
+    rbind(matrix(c(unique(data[, by1]), rep(NA, n_distinct(data[, by1]))),
+                 ncol = 2, dimnames = list(NULL, c(by1, by2)))) %>% 
+    arrange_(by1, by2) %>%
+    mapply(function(x, y) paste(x, y, sep = "="), names(.), .) %>% 
+    apply(., 1, paste, collapse = ", ") %>% 
+    ifelse(grepl("NA", .), stringr::str_split_fixed(., ", ", 2)[, 1], .)
   
   # Compute numerical summaries
   if (!is.null(num.var)) {
-    num.form <- as.formula(paste(paste(num.var, collapse = " + "), "~",
-                                 paste(by1, by2, sep = " + ")))
-    num.form.tot <- as.formula(paste(paste(num.var, collapse = " + "), "~", by1))
-    num.values <- doBy::summaryBy(num.form, num.dat, FUN = contSumFunc,
-                                  digits = digits, stats = stats) %>% 
+    num.ord <- unlist(lapply(num.var, function(x)
+      paste0(x, c("", paste0(".", stats)))))
+    if (all(c("mean", "sd") %in% stats)) {
+      stats <- stats[-grep("sd", stats)]
+      num.ord <- num.ord[-grep("sd", num.ord)]
+    }
+    num.val <- doBy::summaryBy(as.formula(
+      paste(paste(num.var, collapse = " + "), "~", paste(by1, by2, sep = " + "))),
+      num.dat, FUN = contSumFunc, digits = digits, stats = stats) %>% 
       mutate_each(funs(as.character))
-    num.values.tot <- doBy::summaryBy(num.form.tot, num.dat, FUN = contSumFunc,
-                                      digits = digits, stats = stats) %>% 
-      cbind(matrix(NA, dimnames = list(NULL, by2))) %>% 
-      extract(, c(1, ncol(.), 2:(ncol(.) - 1))) %>% 
-      rbind(num.values, .) %>% 
-      arrange_(by1)
-    num.values.long <- num.values.tot %>% 
+    num.val.tot <- doBy::summaryBy(as.formula(
+      paste(paste(num.var, collapse = " + "), "~", by1)),
+      num.dat, FUN = contSumFunc, digits = digits, stats = stats) %>% 
+      mutate_each(funs(as.character))
+    num.all <- data.table::rbindlist(list(num.val, num.val.tot), fill = TRUE)
+    num.long <- num.all %>% 
       tidyr::gather(stat, value, -match(c(by1, by2), names(.))) %>%
       tidyr::separate(stat, c("var", "stat"), "\\.") %>%
       arrange_(by1, by2)
-    facs <- num.values.tot[, c(by1, by2)]
-    num.result <- matrix(num.values.long$value, ncol = nrow(facs)) %>% 
-      rbind(matrix("", nrow = length(num.var), ncol = nrow(facs))) %>% 
-      set_rownames(c(grep(paste(num.var, collapse = "|"), names(num.values),
-                          value = TRUE), num.var)) %>% 
-      extract(row.order, ) %>% 
+    num.res <- matrix(num.long$value, ncol = nrow(num.all),
+                      dimnames = list(grep(paste(num.var, collapse = "|"),
+                                           names(num.val), value = TRUE),
+                                      col.names)) %>% 
+      rbind(matrix("", nrow = length(num.var), ncol = nrow(num.all),
+                   dimnames = list(num.var))) %>% 
+      extract(num.ord, ) %>% 
       set_rownames(unlist(lapply(paste0("**", num.var, "**"),
-                                 function(x) c(x, stats)))) %>% 
-      set_colnames(mapply(function(x, y) paste(x, y, sep = "="),
-                          names(facs), facs[order(facs[, by1]), ]) %>%
-                     apply(., 1, function(x) paste(x, collapse = ", ")) %>% 
-                     ifelse(grepl("NA", .), 
-                            stringr::str_split_fixed(., ", ", 2)[, 1], .))
+                                 function(x) c(x, stats))))
   } else {
-    num.result <- num.values.long <- NULL
+    num.res <- num.long <- NULL
   }
   
   # Compute factor summaries
   if (!is.null(fac.var)) {
-    row.order <- fac.var %>% 
-      mapply(function(x, y) paste0(x, ".", levels(y)), ., fac.dat[, ., drop = FALSE]) %>%
-      rbind(colnames(.), .) %>% 
-      c()
-    fac.values <- fac.var %>% 
-      sapply(function(x) as.formula(paste(x, "~",
-                                          paste(by1, by2, sep = " + ")))) %>%
-      lapply(aggregate, fac.dat, summary) %>% 
+    fac.ord <- c(mapply(function(x, y) paste0(x, c("", paste0(".", levels(y)))),
+                        fac.var, fac.dat[, fac.var, drop = FALSE]))
+    fac.val <- sapply(fac.var, function(x)
+      as.formula(paste(x, "~", paste(by1, by2, sep = " + ")))) %>%
+      lapply(function(y) as.matrix(aggregate(y, fac.dat, summary))) %>% 
       Reduce(merge, .) %>% 
-      extract(order(.[, by1]), ) %>% 
-      as.matrix() %>% 
       as.data.frame()
-    fac.values.tot <- fac.var %>% 
-      sapply(function(x) as.formula(paste(x, "~",
-                                          paste(by1, sep = " + ")))) %>%
-      lapply(aggregate, fac.dat, summary) %>% 
+    fac.val.tot <- sapply(fac.var, function(x)
+      as.formula(paste(x, "~", paste(by1, sep = " + ")))) %>%
+      lapply(function(y) as.matrix(aggregate(y, fac.dat, summary))) %>% 
       Reduce(merge, .) %>% 
-      extract(order(.[, by1]), ) %>% 
-      as.matrix() %>% 
+      as.data.frame()
+    fac.all <- data.table::rbindlist(list(fac.val, fac.val.tot), fill = TRUE) %>%
+      as.data.frame()
+    fac.pct <- fac.val %>% 
+      select(-one_of(by1, by2)) %>% 
+      t() %>% 
       as.data.frame() %>% 
-      cbind(matrix(rep(NA, nrow(.)), dimnames = list(NULL, by2))) %>% 
-      extract(, c(1, ncol(.), 2:(ncol(.) - 1)))
-    fac.values.res <- fac.values.tot %>% 
+      mutate(grp = stringr::str_split_fixed(rownames(.), "\\.", 2)[, 1]) %>% 
+      group_by(grp) %>% 
+      do(vars = rowColPercent(.[, -ncol(.)])) %>%
+      extract(match(.$grp, fac.var), ) %>% 
+      use_series(vars) %>% 
+      lapply(function(x) x %>% 
+               cbind(lev = rep(letters[1:(nrow(x) / 3)], each = 3)) %>% 
+               as.data.frame() %>% 
+               group_by(lev) %>%
+               by(.$lev, function(x) apply(x[, -ncol(.)], 2, pandoc_pcts)) %>% 
+               do.call(rbind, .)) %>% 
+      do.call(rbind, .)
+    fac.pct.tot <- fac.val.tot %>% 
       split(.[, by1]) %>% 
       lapply(function(x) x %>% 
-               select(-one_of(by1, by2)) %>% 
+               select(-matches(by1)) %>% 
                t() %>% 
                as.data.frame() %>% 
                mutate(grp = factor(stringr::str_split_fixed(
@@ -135,71 +152,42 @@ SummaryStatsBy <- function(data, by1, by2, var.names,
                by(.$lev, function(x) apply(x[, -ncol(.)], 2, pandoc_pcts)) %>% 
                do.call(rbind, .)) %>% 
       do.call(rbind, .)
-    facs <- rbind(fac.values, fac.values.tot, make.row.names = FALSE) %>%
-      select_(by1, by2)
-    fac.result <- fac.values %>% 
-      select(-one_of(by1, by2)) %>% 
-      t() %>% 
-      as.data.frame() %>% 
-      mutate(grp = stringr::str_split_fixed(rownames(.), "\\.", 2)[, 1]) %>% 
-      group_by(grp) %>% 
-      do(vars = rowColPercent(.[, -ncol(.)])) %>%
-      extract(match(.$grp, fac.var), ) %>% 
-      use_series(vars) %>% 
-      lapply(function(x) x %>% 
-               cbind(lev = rep(letters[1:(nrow(x) / 3)], each = 3)) %>% 
-               as.data.frame() %>% 
-               group_by(lev) %>%
-               by(.$lev, function(x) apply(x[, -ncol(.)], 2, pandoc_pcts)) %>% 
-               do.call(rbind, .)) %>% 
-      do.call(rbind, .) %>% 
-      cbind(fac.values.res) %>% 
-      rbind(t(facs), .) %>% 
-      extract(, order(.[by1, ])) %>% 
-      set_rownames(colnames(fac.values.tot)) %>%
-      rbind(matrix(rep("", nrow(facs) * length(fac.var)),
+    fac.res <- cbind(fac.pct, fac.pct.tot) %>% 
+      set_rownames(tail(names(fac.val), -2)) %>%
+      rbind(matrix(rep("", nrow(fac.all) * length(fac.var)),
                    nrow = length(fac.var), dimnames = list(fac.var, NULL))) %>%
-      extract(row.order, ) %>% 
+      extract(fac.ord, order(fac.all[, by1])) %>% 
       set_rownames(stringr::str_replace_all(
         rownames(.),
         c(setNames(c(rep("", length(fac.var))), paste0(fac.var, ".")),
           setNames(paste0("**", fac.var, "**"), fac.var)))) %>% 
-      set_colnames(mapply(function(x, y) paste(x, y, sep = "="),
-                          names(facs), facs[order(facs[, by1]), ]) %>% 
-                     apply(., 1, function(x) paste(x, collapse = ", ")) %>%
-                     ifelse(grepl("NA", .),
-                            stringr::str_split_fixed(., ", ", 2)[, 1], .))
-    fac.values.long <- fac.values.tot %>% 
-      as.data.frame() %>% 
+      set_colnames(col.names)
+    fac.long <- fac.all %>% 
       tidyr::gather(stat, value, -match(c(by1, by2), colnames(.))) %>% 
       tidyr::separate(stat, c("var", "stat"), "\\.") %>%
       arrange_(by1, by2)
   } else {
-    fac.result <- fac.values.long <- NULL
+    fac.res <- fac.long <- NULL
   }
 
   # Final results in each format
-  final.result <- rbind(num.result, fac.result)
-  ind <- grep("\\*", rownames(final.result))
-  org.ord <- gsub("\\*\\*", "", rownames(final.result)[ind])
-  
-  final.pandoc <- final.result %>% 
-    extract(mapply(rep, match(org.ord, var.names),
-                   diff(c(ind, nrow(.) + 1))) %>% 
-              unlist() %>% 
-              factor() %>% 
-              order(), )
-  final.html <- final.result %>% 
+  final.res <- rbind(num.res, fac.res)
+  ind <- grep("\\*", rownames(final.res))
+  org.ord <- gsub("\\*\\*", "", rownames(final.res)[ind])
+  final.reord <- final.res %>% 
+    extract(order(unlist(mapply(rep, match(org.ord, var.names),
+                   diff(c(ind, nrow(.) + 1))))), )
+  final.html <- final.res %>% 
     set_rownames(stringr::str_replace_all(
       rownames(.), c("^\\*\\*" = "<b>", "\\*\\*$" = "</b>")))
-  final.values.long <- rbind(num.values.long, fac.values.long) %>% 
+  final.long <- rbind(num.long, fac.long) %>% 
     extract(order(match(.$var, var.names)), )
   final.return <- switch(match.arg(format),
-                         raw = final.result,
+                         raw = final.reord,
                          pandoc = pander::pandoc.table.return(
-                           final.pandoc, emphasize.rownames = FALSE),
+                           final.reord, emphasize.rownames = FALSE),
                          html = htmlTable::htmlTable(final.html),
-                         long = final.values.long)
+                         long = final.long)
   return(final.return)
 }
 
