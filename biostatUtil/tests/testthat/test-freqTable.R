8a2cf0ec945af106e16d161cca83533189b8b7cc
Steven McKinney <smckinney@bccrc.ca>
2016-09-07 19:17:42 -0700

    Initial test for freqTable - SM

---
+++ biostatUtil/tests/testthat/test-freqTable.R
@@ -0,0 +1,13 @@
+
+context("Generating frequency tables and histograms")
+# Create vector of randomly reordered alphabet with various frequencies
+# for each letter
+set.seed(123)
+n <- sample(10, length(letters), replace = TRUE)
+x <- sample(rep.int(letters, times = n))
+
+test_that("Score label and description label match up", {
+  ftdf <- freqTable(x, levels = letters, missing = "x", description = letters, round = 3, plot = TRUE)
+  idxp <- ftdf[, "Score"] != "Total" & ftdf[, "Class"] != "Total"
+  expect_identical(ftdf[idxp, "Score"], ftdf[idxp, "Description"])
+})
