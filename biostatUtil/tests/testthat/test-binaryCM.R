f51d558bba27c110dac7eb00a466b07fedde16b6
Derek Chiu <dchiu@bccrc.ca>
2016-09-12 16:00:44 -0700

    add tests to binaryCM, use test_that infrastructure

--- biostatUtil/tests/testthat/test-binaryCM.R
+++ biostatUtil/tests/testthat/test-binaryCM.R
@@ -1,55 +1,76 @@
-library(testthat)
-# Aline Talhouk
-# The test is performed on the binaryCM function using the same example from that function
 
-test_binaryCM <- function(x,y, pcond){
-bcm <- binaryCM(x, y, pcond = pcond)
-# Check that entering a categorical variable with more than 2 categories will result in an error
-z <-
-  apply(rmultinom(n , size = 1, prob = c(0.2, 0.4, 0.4)), 2, function(x) {
-    which(x == 1)
-  })
-expect_error(binaryCM(x, z))
-
-# Check that the confusion Matrix is such that positive condition is in the (1,1) position of the table
-expect_equal(sum(x == pcond & y == pcond), bcm$CM[1,1])
-
-# Check that the output of the table corresponds to the individual elements
-expect_true(all(bcm$table["Accuracy",] == bcm$Accuracy))
-expect_true(all(bcm$table["Sensitivity",] == bcm$Sensitivity))
-expect_true(all(bcm$table["Specificity",] == bcm$Specificity))
-expect_true(all(bcm$table["PPV",] == bcm$PPV))
-expect_true(all(bcm$table["NPV",] == bcm$NPV))
-expect_true(all(bcm$table["kappa",] == bcm$kappa))
-
-# Check that the point estimate is included in the 95 bootstrap interval
-expect_true(all(bcm$table[, "Point Estimate"] > bcm$table[, "Lower CI"]) &
-              all(bcm$table[, "Point Estimate"] < bcm$table[, "Upper CI"]))
-
-# Check that the accuracy and a PPV and Specificity are correctly calculated
-
-## Defined as sum of diagonal over total
-expect_equal(bcm$Accuracy[, "PointEst"], (sum(x==pcond & y==pcond)+ sum(x!=pcond &y!=pcond)) / length(x))
-
-## PPV is defined as sum of true positives/ predicted positive
-expect_equal(bcm$PPV[,"PointEst"],sum(x == pcond & y == pcond) / sum(y == pcond), tolerance = .0002)
-
-## Specificity is defined as sum of true negatives/ sum of true condition negative
-expect_equal(bcm$Specificity[,"PointEst"],sum(x != pcond & y != pcond) / sum(x != pcond), tolerance = .0002)
-}
-
+context("Binary class confusion matrices")
 
 set.seed(547)
 n <- 80
+tol <- .0002
 x <- rbinom(n, size = 1, prob = 0.6) # Ref
-xf <- factor(x, labels = c("positive","negative"))
+xf <- factor(x, labels = c("positive", "negative"))
 
 y <- rbinom(n, size = 1, prob = 0.4) # Pred
-yf <- factor(y, labels = c("negative","positive"))
+yf <- factor(y, labels = c("negative", "positive"))
 
-pcondf <- "positive"
-# Check that it works for numeric values (0/1)
-test_binaryCM(x,y,pcond = 1)
+expect_binaryCM_success <- function(x, y, pcond) {
+  bcm <- binaryCM(x, y, pcond = pcond)
+  z <- apply(rmultinom(n , size = 1, prob = c(0.2, 0.4, 0.4)), 2,
+             function(x) which(x == 1))
+  
+  test_that("entering a categorical variable with more than 2 categories will result in an error", {
+    expect_error(binaryCM(x, z))
+  })
+  
+  test_that("the confusion Matrix is such that positive condition is in the (1,1) position of the table", {
+    expect_equal(sum(x == pcond & y == pcond), bcm$CM[1, 1])
+  })
+  
+  test_that("the output of the table corresponds to the individual elements", {
+    expect_true(all(bcm$table["Accuracy", ] == bcm$Accuracy))
+    expect_true(all(bcm$table["Sensitivity", ] == bcm$Sensitivity))
+    expect_true(all(bcm$table["Specificity", ] == bcm$Specificity))
+    expect_true(all(bcm$table["PPV", ] == bcm$PPV))
+    expect_true(all(bcm$table["NPV", ] == bcm$NPV))
+    expect_true(all(bcm$table["kappa", ] == bcm$kappa))
+  })
+  
+  test_that("the point estimate is included in the 95 bootstrap interval", {
+    expect_true(all(bcm$table[, "Point Estimate"] > bcm$table[, "Lower CI"]) &
+                  all(bcm$table[, "Point Estimate"] < bcm$table[, "Upper CI"]))
+  })
+  
+  test_that("accuracy is sum of diagonal over total", {
+    expect_equal(bcm$Accuracy[, "PointEst"],
+                 (sum(x == pcond & y == pcond) + sum(x != pcond & y != pcond)) / length(x))
+  })
+  
+  test_that("sensitivity is defined as sum of true positives / sum of true condition positive", {
+    expect_equal(bcm$Sensitivity[, "PointEst"],
+                 sum(x == pcond & y == pcond) / sum(x == pcond), tolerance = tol)
+  })
+  
+  test_that("specificity is defined as sum of true negatives / sum of true condition negative", {
+    expect_equal(bcm$Specificity[, "PointEst"],
+                 sum(x != pcond & y != pcond) / sum(x != pcond), tolerance = tol)
+  })
+  
+  test_that("PPV is defined as sum of true positives / predicted positive", {
+    expect_equal(bcm$PPV[, "PointEst"],
+                 sum(x == pcond & y == pcond) / sum(y == pcond), tolerance = tol)
+  })
+  
+  test_that("NPV is defined as sum of true negatives / predicted negative", {
+    expect_equal(bcm$NPV[, "PointEst"],
+                 sum(x != pcond & y != pcond) / sum(y != pcond), tolerance = tol)
+  })
+}
 
-# Check that it works with factors and labels
-test_binaryCM(xf,yf,pcond = "positive")
+test_that("binaryCM works for numeric values (0/1)", {
+  expect_binaryCM_success(x, y, pcond = 1)
+})
+
+test_that("binaryCM works with factors and labels", {
+  expect_binaryCM_success(xf, yf, pcond = "positive")  
+})
+
+test_that("outputs can print to console", {
+  invisible(capture.output(binaryCM(x, y, verbose = TRUE)))
+})
