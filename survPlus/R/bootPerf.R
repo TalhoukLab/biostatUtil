dbfce1038f8bea73f57bdd7cf8e8e516378deee2
Derek Chiu <dchiu@bccrc.ca>
2016-09-14 12:07:46 -0700

    correct syntax for roxygen lists is \item{name}{description}

--- survPlus/R/bootPerf.R
+++ survPlus/R/bootPerf.R
@@ -1,6 +1,8 @@
 #' Assess Performance of a survival model
 #'
-#' Produce metrics to evaluate a survival model in Bootstrap samples (Internal Validation)
+#' Produce metrics to evaluate a survival model in Bootstrap samples (Internal
+#' Validation)
+#'
 #' @param dat0 is the training data
 #' @param var.names are the names of the predictors
 #' @param is the time variable
@@ -9,38 +11,48 @@
 #' @param seed is a random number generator seed set to 2014
 #' @param firth whether or not to perform a firth correction set to TRUE
 #' @return
-#' \item{apparent} the apparent fit: testing the model on the full training data
-#' \item{boot} the bootstrap fit: testing the model on the bootstrapped training data
-#' \item{boot632} the bootstrap 0.632 fit: testing the model on the bootstrapped test data, ie the data that was not selected for bootstrap training.
+#' \item{apparent}{the apparent fit: testing the model on the full training
+#' data}
+#' \item{boot}{the bootstrap fit: testing the model on the bootstrapped training
+#' data}
+#' \item{boot632}{the bootstrap 0.632 fit: testing the model on the bootstrapped
+#' test data, ie the data that was not selected for bootstrap training}
 #' @author Aline Talhouk
 #' @export
-
-
-bootPerf <- function(dat0, var.names, time.var, event.ind, B=1000, seed=2014, firth=TRUE){
+bootPerf <- function(dat0, var.names, time.var, event.ind,
+                     B = 1000, seed = 2014, firth = TRUE) {
   set.seed(seed)
 
   #Select relevant variables and remove variables with missing observations
-  dat <- na.omit(dat0[,c(time.var,event.ind,var.names)])
+  dat <- na.omit(dat0[, c(time.var, event.ind, var.names)])
   #Generate boostrap samples
-  boots <- genBootSpls(dat,B)
+  boots <- genBootSpls(dat, B)
 
   #Construct modeling formula
-  mod.form <- as.formula(paste("survival::Surv(",time.var,",",event.ind,")" ,"~", paste(var.names, collapse = " + ")))
+  mod.form <- as.formula(paste("survival::Surv(", time.var, ",", event.ind,
+                               ")" , "~", paste(var.names, collapse = " + ")))
 
   #Apparent fit (testing the model on the full training data (dat))
   app.mod.fit <- survPlus::doCox(mod.form, data = dat, firth = firth)
   perf.app <- survPlus::Cindex(app.mod.fit, dat)
 
   #Bootstrap fit
-  boot.mod<- lapply(boots$boot.tr, function(x) survPlus::doCox(mod.form,data = x, firth = firth))
+  boot.mod <- lapply(boots$boot.tr, function(x)
+    survPlus::doCox(mod.form, data = x, firth = firth))
 
   ## Standard bootstrap (test the model on the bootstrapped training data)
-  perf.boot <- mapply(function(X,Y){Cindex(X, newdat0=Y)},
-                             X = boot.mod,Y = boots$boot.tr, SIMPLIFY = F)
+  perf.boot <- mapply(function(X, Y) {
+    Cindex(X, newdat0 = Y)
+  },
+  X = boot.mod, Y = boots$boot.tr, SIMPLIFY = FALSE)
+
   ## .632 bootstrap (test the model on the bootstrapped test data)
-  perf.632 <-mapply(function(X,Y){Cindex(X, newdat=Y)},
-                    X = boot.mod,Y = boots$boot.te, SIMPLIFY = F)
+  perf.632 <- mapply(function(X, Y) {
+    Cindex(X, newdat = Y)
+  },
+  X = boot.mod, Y = boots$boot.te, SIMPLIFY = FALSE)
 
-  return(list(apparent=perf.app, boot=unlist(perf.boot), boot632=unlist(perf.632)))
+  return(list(apparent = perf.app,
+              boot = unlist(perf.boot),
+              boot632 = unlist(perf.632)))
 }
-
--- survPlus/R/decomposeSurvform.R
+++ survPlus/R/decomposeSurvform.R
@@ -1,39 +1,44 @@
-#'An internal function that decomposes a survival formula
-#'@param formula a formula object, with the response on the left of a ~ operator, and the terms on the right. The response must be a survival object as returned by the Surv function.
-#'@param data a data.frame in which to interpret the variables named in the formula.
-#'@return This function retuns a list with the following parameters
-#'\item{resp} Surv response matrix
-#'\item{mm1} Model matrix without time effects
-#'\item{survpair} names of survival pairs
-#'\item{predictors} names of covariates
-#'\item{indcc} indicator of complete cases
-#'@author Aline Talhouk (adapted from an existing function)
-
-decomposeSurvform <-function(formula,data){
+#' An internal function that decomposes a survival formula
+#' @param formula a formula object, with the response on the left of a ~ operator, and the terms on the right. The response must be a survival object as returned by the Surv function.
+#' @param data a data.frame in which to interpret the variables named in the formula.
+#' @return This function retuns a list with the following parameters
+#' \item{resp}{Surv response matrix}
+#' \item{mm1}{Model matrix without time effects}
+#' \item{survpair}{ames of survival pairs}
+#' \item{predictors}{names of covariates}
+#' \item{indcc}{indicator of complete cases}
+#' @author Aline Talhouk (adapted from an existing function)
+#' @export
+decomposeSurvform <- function(formula, data) {
     # save the entered formula
     orig.formula <- formula
     ## construct response variable:
     SurvResp <- model.extract(model.frame(formula, data = data), "response")
     mm <- model.matrix(formula, data = data) ## Model-Matrix
-    mm1 <- mm[, -1, drop=FALSE]	# w/o intercept
-    terms <- terms(formula, data=data)
+    mm1 <- mm[,-1, drop = FALSE]	# w/o intercept
+    terms <- terms(formula, data = data)
     fac <- attr(terms, "factors")
     predictors <- attr(terms, "term.labels")
     labels <- attr(terms, "term.labels")
     ## function that splits by special chars
-    f<-function(str) {
-      for(chars in c("(", ")", ":", " ", ",", "*", "^"))
-        str <- unlist(strsplit(str, split=chars, fixed=TRUE))
+    f <- function(str) {
+      for (chars in c("(", ")", ":", " ", ",", "*", "^"))
+        str <- unlist(strsplit(str, split = chars, fixed = TRUE))
       str
     }
     rowSplit <- sapply(rownames(fac), f)	# splitted effects
     time <- tail(rowSplit[[1]], 2)[1]	# name of stoptime
     event <- tail(rowSplit[[1]], 2)[2]	# name of stoptime
-    indcc <- complete.cases(data[,c(time, event, predictors)])
-    list(resp=SurvResp, 			# Surv response matrix
-         mm1=mm1, 			# model matrix without time effects
-         survpair=c(time,event),# names of survival pairs
-         predictors=predictors,	# names of covariates
-         indcc=indcc			# indicator of complete cases
+    indcc <- complete.cases(data[, c(time, event, predictors)])
+    list(
+      resp = SurvResp,
+      # Surv response matrix
+      mm1 = mm1,
+      # model matrix without time effects
+      survpair = c(time, event),
+      # names of survival pairs
+      predictors = predictors,
+      # names of covariates
+      indcc = indcc			# indicator of complete cases
     )
-    }
+}
