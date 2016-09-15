f988ede9f164c24890d9c3b7bdd5bf26a6960f5c
Samuel Leung <sam.leung@gmail.com>
2016-09-15 13:32:42 -0700

    added option "digits" to adjust # of digits to round off

--- survPlus/R/Cindex.R
+++ survPlus/R/Cindex.R
@@ -1,8 +1,9 @@
 #' Compute the Cindex
-#'
+#' 
+#' @param digits to round the Cindex; if digits=NULL, do not round
 #' Produce metrics to evaluate a survival model
 #' @export
-Cindex <- function(mod, newdat0){
+Cindex <- function(mod, newdat0, digits=3){
   # extract components
   surv.form <- unlist(strsplit(as.character(mod$formula), split='~')[2])
   obj <- decomposeSurvform(mod$formula, newdat0)
@@ -17,6 +18,9 @@ Cindex <- function(mod, newdat0){
 
   C <- Hmisc::rcorrcens(survival::Surv(newdat[,surv[1]],newdat[,surv[2]]) ~ I(-1 * pred))
 
- return(c(round(C[1],3)))
-
+  if (is.null(digits)) {
+    return(C[1])
+  } else {
+    return(c(round(C[1],digits)))
+  }
 }
