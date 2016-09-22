0f4ff492cfaadbee7d7f85d4d99f18f06a92be9b
Derek Chiu <dchiu@bccrc.ca>
2016-09-22 12:29:48 -0700

    group cleanDate, formatDate, getFormat, numericToDate as date_format

--- biostatUtil/tests/testthat/test-date_format.R
+++ biostatUtil/tests/testthat/test-date_format.R
@@ -1,10 +1,15 @@
 
-context("Date functions")
+context("Date formatting functions")
 
 x1 <- "2014-07-08"
 x2 <- "2014/07/08"
 x3 <- "2014|07|08"
 x4 <- "20140708"
+
+test_that("Wrapper around numeric method for as.Date uses 1970 as default origin", {
+  expect_identical(as.Date("1970-01-11"), numericToDate(10))
+  expect_identical(as.Date("2000-09-21"), numericToDate(10, "2000-09-11"))
+})
 
 test_that("Any date format works with any separator", {
   expect_identical("%m-%d-%Y", getFormat(x1, "MM.DD.YYYY"))
@@ -66,46 +71,4 @@ test_that("Reformatting dates handles erroneous inputs", {
   expect_equal("Missing", cleanDate(NA, return.missing.code = "Missing"))
   expect_equal(x4, cleanDate(x4, existing.missing.codes = x4))
   expect_equal(NA, cleanDate("This is not a date"))
-})
-
-test_that("Adding date works for allowed units", {
-  expect_identical("2014/07/18", addToDate(x2, 10, date.format = "YYYY.MM.DD", units = "days"))
-  expect_identical("2014/09/16", addToDate(x2, 10, date.format = "YYYY.MM.DD", units = "weeks"))
-  expect_identical("2015/05/09", addToDate(x2, 10, date.format = "YYYY.MM.DD", units = "months"))
-  expect_identical("2024/07/07", addToDate(x2, 10, date.format = "YYYY.MM.DD", units = "years"))
-})
-
-test_that("Adding date handles erroneous inputs", {
-  expect_equal(NA, addToDate(NA, 10))
-  expect_equal(NA, addToDate(x2, NA))
-  expect_equal("Missing", addToDate(x2, 10, existing.missing.codes = x2, return.missing.code = "Missing"))
-})
-
-x5 <- "1992/01/27"
-x6 <- "2003/03/21"
-
-test_that("Subtracting date works for allowed units", {
-  expect_equal(4071, diffDate(x6, x5, date.format = "YYYY.MM.DD", units = "days"))
-  expect_equal(581.5714, diffDate(x6, x5, date.format = "YYYY.MM.DD", units = "weeks"), tolerance = .002)
-  expect_equal(133.4754, diffDate(x6, x5, date.format = "YYYY.MM.DD", units = "months"), tolerance = .002)
-  expect_equal(11.14607, diffDate(x6, x5, date.format = "YYYY.MM.DD", units = "years"), tolerance = .002)
-})
-
-test_that("Subtracting date handles erroneous inputs", {
-  expect_equal(NA, diffDate(x6, NA))
-  expect_equal(NA, diffDate(NA, x5))
-  expect_equal("Missing", diffDate(x6, x5, existing.missing.codes = x5, return.missing.code = "Missing"))
-  expect_equal("Missing", diffDate(x6, x5, existing.missing.codes = x6, return.missing.code = "Missing"))
-})
-
-test_that("Comparing dates works for any two valid dates", {
-  expect_equal(-1, compareDate("01/22/1949", "04/13/1950", date.format = "MM.DD.YYYY"))
-  expect_equal(0, compareDate("04/13/1950", "04/13/1950", date.format = "MM.DD.YYYY"))
-  expect_equal(1, compareDate("04/13/1959", "04/13/1950", date.format = "MM.DD.YYYY"))
-  expect_equal(NA, compareDate(NA, "04/13/1950", date.format = "MM.DD.YYYY"))
-})
-
-test_that("Wrapper around numeric method for as.Date uses 1970 as default origin", {
-  expect_identical(as.Date("1970-01-11"), numericToDate(10))
-  expect_identical(as.Date("2000-09-21"), numericToDate(10, "2000-09-11"))
 })
