ce46a044b5f047fb59bdac092b9269e98dc1c555
Derek Chiu <dchiu@bccrc.ca>
2016-09-23 16:26:23 -0700

    add pole-specific plotting funs

--- biostatUtil/R/pole_funs.R
+++ biostatUtil/R/pole_funs.R
@@ -164,3 +164,158 @@ doKMPlotsPOLEInitTrt <- function(data, surv.type,
     single.test.type = "none", surv.type = surv.type, 
     conf.int = TRUE, use.aline.plot = use.aline.plot, ...)
 }
+
+#' Grade trend barplot
+#' 
+#' @param input.d input \code{data.frame}
+#' @param title title of barplot
+#' @param legend.space amount of space between legend and edge of plot
+#' @param use.prop logical; if \code{TRUE}, proportions are used
+#' @return a barplot showing the change of Grade over time
+#' @author Samuel Leung
+#' @export
+doGrdTrendBarplot <- function(input.d, title, legend.space = 0.2,
+                              use.prop = TRUE) {
+  legend.space <- round(legend.space * nrow(input.d))
+  grade.trend <- grade.trend.n <- c()
+  temp.d <- input.d
+  dx.years <- names(table(temp.d$dx.year))
+  for (dx.year in dx.years) {
+    select.cases <- temp.d$dx.year == dx.year
+    n <- length(temp.d$Tumour.Grade[select.cases])
+    grade1 <- sum(temp.d$Tumour.Grade[select.cases] == "Grade 1")	
+    grade2 <- sum(temp.d$Tumour.Grade[select.cases] == "Grade 2")
+    grade3 <- sum(temp.d$Tumour.Grade[select.cases] == "Grade 3")
+    grade <- c(grade1, grade2, grade3)
+    if (use.prop)
+      grade <- grade / n * 100
+    grade.trend <- cbind(grade.trend, grade)
+    grade.trend.n <- c(grade.trend.n, n)
+  }
+  colnames(grade.trend) <- dx.years
+  if (length(dx.years) > 20)
+    colnames(grade.trend)[which(rep(c(FALSE, TRUE), 11))] <- ""
+  if (sum(dx.years == 1999) > 0)
+    colnames(grade.trend)[dx.years == 1999] <- ""
+  if (sum(dx.years == 2000) > 0)
+    colnames(grade.trend)[dx.years == 2000] <- 2000
+  if (sum(dx.years == 2001) > 0)
+    colnames(grade.trend)[dx.years == 2001] <- ""
+  rownames(grade.trend) <- c("Grade 1", "Grade 2", "Grade 3")
+  grade.trend <- cbind(grade.trend, rep("", nrow(grade.trend)))
+  colnames(grade.trend)[ncol(grade.trend)] <- ""
+  grade.trend.n <- c(grade.trend.n, legend.space)
+  barplot(grade.trend, width = grade.trend.n, legend.text = T,
+          args.legend = list(x = "right"),
+          col = c("red4", "red3", "red"), las = 2, main = title,
+          ylab = "Percentage of cases", xlab = "Year of diagnosis",
+          sub = "(width of bar proportional to the number of patients)")
+}
+
+#' Survival trend barplot
+#' 
+#' @param input.d input \code{data.frame}
+#' @param title title of barplot
+#' @param surv.yrs vector of survival times in years
+#' @param surv.sts vector of survival statuses
+#' @param surv.event vector of survival events
+#' @param event.desc description of events
+#' @param censor.desc description of censoring
+#' @param obs.yrs number of years to observe survival trend
+#' @param legend.space amount of space between legend and edge of plot
+#' @param use.prop logical; if \code{TRUE}, proportions are used
+#' @return a barplot showing the change of survival outcome over time
+#' @author Samuel Leung
+#' @export
+doSurvTrendBarplot <- function(input.d, title, surv.yrs, surv.sts, surv.event,
+                               event.desc, censor.desc,  obs.yrs = 3,
+                               legend.space = 0.2, use.prop = TRUE) {
+  legend.space <- round(legend.space * nrow(input.d))
+  survival.trend <- survival.trend.n <- c()
+  temp.d <- input.d
+  dx.years <- names(table(temp.d$dx.year))
+  for (dx.year in dx.years) {
+    select.cases <- temp.d$dx.year == dx.year
+    n <- sum(!is.na(temp.d[select.cases, surv.yrs]))
+    event <- sum(temp.d[select.cases, surv.sts] == surv.event &
+                   as.numeric(temp.d[select.cases, surv.yrs]) <= obs.yrs,
+                 na.rm = TRUE)
+    censor <- n - event
+    survival <- c(event, censor)
+    if (use.prop)
+      survival <- survival / n * 100
+    survival.trend <- cbind(survival.trend, survival)
+    survival.trend.n <- c(survival.trend.n, n)
+  }
+  colnames(survival.trend) <- dx.years
+  if (length(dx.years) > 20)
+    colnames(survival.trend)[which(rep(c(FALSE, TRUE), 11))] <- ""
+  if (sum(dx.years == 1999) > 0)
+    colnames(survival.trend)[dx.years == 1999] <- ""
+  if (sum(dx.years == 2000) > 0)
+    colnames(survival.trend)[dx.years == 2000] <- 2000
+  if (sum(dx.years == 2001) > 0)
+    colnames(survival.trend)[dx.years == 2001] <- ""
+  rownames(survival.trend) <- c(event.desc, censor.desc)
+  survival.trend <- cbind(survival.trend, rep("", nrow(survival.trend)))
+  colnames(survival.trend)[ncol(survival.trend)] <- ""
+  survival.trend.n <- c(survival.trend.n, legend.space)
+  barplot(survival.trend, width = survival.trend.n, legend.text = TRUE,
+          args.legend = list(x = "right"), col = c("grey", "pink"),
+          las = 2, main = title,
+          ylab = "Percentage of cases", xlab = "Year of diagnosis",
+          sub = "(width of bar proportional to the number of patients)")
+}
+
+#' Treatment trend barplots
+#' 
+#' @param input.d input \code{data.frame}
+#' @param title title of barplot
+#' @param legend.space amount of space between legend and edge of plot
+#' @param use.prop logical; if \code{TRUE}, proportions are used
+#' @return a barplot showing the change of Treatment over time
+#' @author Samuel Leung
+#' @export
+doTrtTrendBarplot <- function(input.d, title, legend.space = 0.2,
+                              use.prop = TRUE) {
+  legend.space <- round(legend.space * nrow(input.d))
+  treatment.trend <- treatment.trend.n <- c()
+  temp.d <- input.d
+  dx.years <- names(table(temp.d$dx.year))
+  for (dx.year in dx.years) {
+    select.cases <- temp.d$dx.year == dx.year
+    n <- sum(!temp.d$init.treatment[select.cases] %in% ALL.MISSING.CODES)
+    num.chemo.only <- sum(temp.d$init.treatment[select.cases] ==
+                            VALUE.CODING.INIT.TREATMENT.CHEMO.ONLY)	
+    num.rt.only <- sum(temp.d$init.treatment[select.cases] ==
+                         VALUE.CODING.INIT.TREATMENT.RT.ONLY)
+    num.chemo.rt <- sum(temp.d$init.treatment[select.cases] ==
+                          VALUE.CODING.INIT.TREATMENT.BOTH)
+    num.no.tx <- sum(temp.d$init.treatment[select.cases] %in%
+                       c(VALUE.CODING.INIT.TREATMENT.NO,
+                         VALUE.CODING.INIT.TREATMENT.VAG.BRACHY.ONLY))
+    treatment <- c(num.chemo.only, num.rt.only, num.chemo.rt, num.no.tx)
+    if (use.prop)
+      treatment <- treatment / n * 100
+    treatment.trend <- cbind(treatment.trend, treatment)
+    treatment.trend.n <- c(treatment.trend.n, n)
+  }
+  colnames(treatment.trend) <- dx.years
+  if (length(dx.years) > 20)
+    colnames(treatment.trend)[which(rep(c(FALSE, TRUE), 11))] <- ""
+  if (sum(dx.years == 1999) > 0)
+    colnames(treatment.trend)[dx.years == 1999] <- ""
+  if (sum(dx.years == 2000) > 0)
+    colnames(treatment.trend)[dx.years == 2000] <- 2000
+  if (sum(dx.years == 2001) > 0)
+    colnames(treatment.trend)[dx.years == 2001] <- ""
+  rownames(treatment.trend) <- c("chemo only", "rt only", "both", "none")
+  treatment.trend <- cbind(treatment.trend, rep("", nrow(treatment.trend)))
+  colnames(treatment.trend)[ncol(treatment.trend)] <- ""
+  treatment.trend.n <- c(treatment.trend.n, legend.space)
+  barplot(treatment.trend, width = treatment.trend.n, legend.text = TRUE,
+          args.legend = list(x = "right"),
+          col = c("red", "yellow", "brown", "blue"), las = 2, main = title,
+          ylab = "Percentage of cases", xlab = "Year of diagnosis",
+          sub = "(width of bar proportional to the number of patients)")
+}
