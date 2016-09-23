7ae04684e230868912659ec62efbf267deeb88c2
Derek Chiu <dchiu@bccrc.ca>
2016-09-23 12:57:43 -0700

    unit test for pairwiseCor

--- biostatUtil/R/pairwiseCor.R
+++ biostatUtil/R/pairwiseCor.R
@@ -17,11 +17,12 @@ pairwiseCor <- function(dataframe) {
   if (!all(apply(dataframe, 2, is.numeric))) {
     stop("All columns of data matrix must be numeric")
   }
-  . <- Cor <- AbsCor <- Pval <- AdjP <- NULL
+  Cor <- AbsCor <- Pval <- AdjP <- NULL
   x <- dataframe
   pairs <- combn(colnames(dataframe), 2) %>% 
     set_rownames(paste0("Variable", 1:2))
-  pairwiseCorDF <- data.frame(Cor = apply(pairs, 2, function(df) cor(x[, df]))[2, ]) %>% 
+  pairwiseCorDF <- data.frame(Cor = apply(pairs, 2, function(df)
+    cor(x[, df]))[2, ]) %>% 
     mutate(AbsCor = abs(Cor),
            Pval = mapply(function(v1, v2) cor.test(v1, v2)$p.value,
                          x[pairs[1, ]], x[pairs[2, ]]),
