430bf61a91e27b83adef6a5ce824cd5183539d66
Derek Chiu <dchiu@bccrc.ca>
2016-09-26 11:57:54 -0700

    package documentation

---
+++ ConClust/R/ConClust-package.r
@@ -0,0 +1,11 @@
+#' ConClust
+#'
+#' The ConClust package performs Consensus Clustering, an ensemble clustering
+#' algorithm that pools together results from different clustering realizations.
+#' Each realization uses a unique combination of observations and clustering
+#' algorithms.
+#'
+#' @name ConClust
+#' @docType package
+#' @importFrom magrittr set_names set_rownames
+NULL
