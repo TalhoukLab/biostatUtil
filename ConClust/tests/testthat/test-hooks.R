274ce87599d31a2c72c0d3f3af9e51ac3e1a1734
Derek Chiu <dchiu@bccrc.ca>
2016-09-26 11:57:43 -0700

    first batch of unit tests

---
+++ ConClust/tests/testthat/test-hooks.R
@@ -0,0 +1,17 @@
+
+context("Hook functions")
+
+library(cluster)
+data(votes.repub)
+
+test_that("algorithm hooks work", {
+  d <- dist(votes.repub)
+  expect_length(agnes_hook(d, k = 2), nrow(votes.repub))
+  expect_length(diana_hook(d, k = 2), nrow(votes.repub))
+})
+
+test_that("distance hook works", {
+  x <- matrix(rnorm(100), nrow = 5)
+  y <- mi_hook(x)
+  expect_is(y, "dist")
+})
