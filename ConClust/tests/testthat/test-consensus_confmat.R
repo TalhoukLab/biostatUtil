1c9d19cdc4e97b67369dcc466d5fd4fd8109a3ee
Derek Chiu <dchiu@bccrc.ca>
2016-09-27 15:41:31 -0700

    new function names plus unit tests for consensus_class, consensus_confmat, consensus_summary

---
+++ ConClust/tests/testthat/test-consensus_confmat.R
@@ -0,0 +1,9 @@
+
+context("Consensus confusion matrix")
+
+test_that("confusion matrix works as expected", {
+  set.seed(2)
+  a <- sample(1:4, 100, replace = TRUE)
+  b <- sample(1:4, 100, replace = TRUE)
+  consensus_confmat(a, b, pred.lab = "Second Run", ref.lab = "First Run")
+})
