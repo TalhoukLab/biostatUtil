c37857c80522d6542694adfc4dde387e44808e3e
Derek Chiu <dchiu@bccrc.ca>
2016-09-28 15:11:27 -0700

    speed up the computation time a bit

--- ConClust/tests/testthat/test-consensus_combine.R
+++ ConClust/tests/testthat/test-consensus_combine.R
@@ -3,11 +3,11 @@ context("Consensus results combine and compare")
 
 set.seed(911)
 x <- matrix(rnorm(1000), nrow = 10)
-CC1 <- ConClust(x, k = 4, reps = 10, method = "apEucl", save = FALSE)
-CC2 <- ConClust(x, k = 4, reps = 10, method = "gmmBIC", save = FALSE)
+CC1 <- ConClust(x, k = 4, reps = 5, method = "apEucl", save = FALSE)
+CC2 <- ConClust(x, k = 4, reps = 5, method = "gmmBIC", save = FALSE)
 CC1.summ <- consensus_summary(CC1, k = 4)
 CC2.summ <- consensus_summary(CC2, k = 4)
-CCP <- ConClustPlus(x, k = 4, reps = 10, save = FALSE)
+CCP <- ConClustPlus(x, k = 4, reps = 5, save = FALSE)
 
 y1 <- consensus_combine(CC1.summ, CC2.summ, res.CCP = CCP, k = 4,
                         element = "matrix")
