d5242e63763f6c714f7d4e5bd9a257b0996f8094
Derek Chiu <dchiu@bccrc.ca>
2016-09-27 17:26:20 -0700

    all cluster analysis statistics go in stats.R, unit tests added

--- ConClust/tests/testthat/test-accuracy.R
+++
@@ -1,8 +0,0 @@
-
-context("Classification accuracy")
-
-test_that("accuracy works", {
-  set.seed(1)
-  x <- matrix(rbinom(16, 20, 0.4), nrow = 4)
-  expect_equal(sum(diag(x)) / sum(x), accuracy(x))
-})
