df94d653d19aa3460e183b48d0ef75fe7ee5da83
Derek Chiu <dchiu@bccrc.ca>
2016-10-05 12:49:54 -0700

    coverage didn't count without expectations

--- biostatUtil/tests/testthat/test-percents_html.R
+++ biostatUtil/tests/testthat/test-percents_html.R
@@ -7,45 +7,44 @@ B <- matrix(rbinom(20, size = 20, prob = 0.3), nrow = 5,
             dimnames = list(paste0("Row", 1:5), paste0("Col", 1:4)))
 
 test_that("HTML output works", {
-  htmlTable(colPercentAsHTML(B, keep = TRUE))
-  htmlTable(colPercentAsHTML(B, keep = FALSE))
-  htmlTable(rowPercentAsHTML(B, keep = TRUE))
-  htmlTable(rowPercentAsHTML(B, keep = FALSE))
-  htmlTable(rowColPercentAsHTML(B, keep = TRUE))
-  htmlTable(rowColPercentAsHTML(B, keep = FALSE))
+  expect_error(htmlTable(colPercentAsHTML(B, keep = TRUE)), NA)
+  expect_error(htmlTable(colPercentAsHTML(B, keep = FALSE)), NA)
+  expect_error(htmlTable(rowPercentAsHTML(B, keep = TRUE)), NA)
+  expect_error(htmlTable(rowPercentAsHTML(B, keep = FALSE)), NA)
+  expect_error(htmlTable(rowColPercentAsHTML(B, keep = TRUE)), NA)
+  expect_error(htmlTable(rowColPercentAsHTML(B, keep = FALSE)), NA)
 })
 
 test_that("Banded rows alternate per row group", {
-  htmlTable(colPercentAsHTML(B, keep = TRUE, banded.rows = TRUE))
-  htmlTable(colPercentAsHTML(B, keep = FALSE, banded.rows = TRUE))
-  htmlTable(rowPercentAsHTML(B, keep = TRUE, banded.rows = TRUE))
-  htmlTable(rowPercentAsHTML(B, keep = FALSE, banded.rows = TRUE))
-  htmlTable(rowColPercentAsHTML(B, keep = TRUE, banded.rows = TRUE))
-  htmlTable(rowColPercentAsHTML(B, keep = FALSE, banded.rows = TRUE))
+  expect_error(htmlTable(colPercentAsHTML(B, keep = TRUE, banded.rows = TRUE)), NA)
+  expect_error(htmlTable(colPercentAsHTML(B, keep = FALSE, banded.rows = TRUE)), NA)
+  expect_error(htmlTable(rowPercentAsHTML(B, keep = TRUE, banded.rows = TRUE)), NA)
+  expect_error(htmlTable(rowPercentAsHTML(B, keep = FALSE, banded.rows = TRUE)), NA)
+  expect_error(htmlTable(rowColPercentAsHTML(B, keep = TRUE, banded.rows = TRUE)), NA)
+  expect_error(htmlTable(rowColPercentAsHTML(B, keep = FALSE, banded.rows = TRUE)), NA)
 })
 
 test_that("Other formatting options work", {
-  htmlTable(rowPercentAsHTML(B, caption = "Test Caption"))
-  htmlTable(colPercentAsHTML(B, html.table.border = 10))
-  htmlTable(rowColPercentAsHTML(B, keep = FALSE, banded.rows = TRUE,
-                                col.odd = "yellow", col.even = "green"))
+  expect_error(htmlTable(rowPercentAsHTML(B, caption = "Test Caption")), NA)
+  expect_error(htmlTable(colPercentAsHTML(B, html.table.border = 10)), NA)
+  expect_error(htmlTable(rowColPercentAsHTML(B, keep = FALSE, banded.rows = TRUE,
+                                             col.odd = "yellow", col.even = "green")), NA)
 })
 
 test_that("Row and column names can be overwritten", {
-  htmlTable(colPercentAsHTML(B, keep = TRUE, row.names = letters[1:5],
-                             column.names = LETTERS[1:4]))
-  htmlTable(rowPercentAsHTML(B, keep = TRUE, row.names = letters[1:5],
-                             column.names = LETTERS[1:4]))
-  htmlTable(rowColPercentAsHTML(B, keep = TRUE, row.names = letters[1:5],
-                                column.names = LETTERS[1:4]))
-
+  expect_error(htmlTable(colPercentAsHTML(B, keep = TRUE, row.names = letters[1:5],
+                                          column.names = LETTERS[1:4])), NA)
+  expect_error(htmlTable(rowPercentAsHTML(B, keep = TRUE, row.names = letters[1:5],
+                                          column.names = LETTERS[1:4])), NA)
+  expect_error(htmlTable(rowColPercentAsHTML(B, keep = TRUE, row.names = letters[1:5],
+                                             column.names = LETTERS[1:4])), NA)
 })
 
 test_that("Transpose can be useful", {
   set.seed(13)
   C <- matrix(rbinom(10, 10, 0.4), nrow = 1)
   D <- matrix(rbinom(10, 10, 0.4), ncol = 1)
-  htmlTable(rowPercentAsHTML(C, transpose = FALSE))
-  htmlTable(rowPercentAsHTML(C, transpose = TRUE))
-  htmlTable(colPercentAsHTML(D, transpose = TRUE))
+  expect_error(htmlTable(rowPercentAsHTML(C, transpose = FALSE)), NA)
+  expect_error(htmlTable(rowPercentAsHTML(C, transpose = TRUE)), NA)
+  expect_error(htmlTable(colPercentAsHTML(D, transpose = TRUE)), NA)
 })
