899be835c3dd9c346250cb228f398a088e95bb6c
Derek Chiu <dchiu@bccrc.ca>
2016-10-05 12:50:14 -0700

    92% coverage for unit test

--- biostatUtil/tests/testthat/test-doCohortCharacteristics.R
+++ biostatUtil/tests/testthat/test-doCohortCharacteristics.R
@@ -1,18 +1,84 @@
 
 context("Compute cohort characteristics")
 
-test_that("function returns four different results", {
+library(htmlTable)
+mtcars$vs <- as.factor(mtcars$vs)
+
+test_that("defaults can be changed and output is a length 4 list", {
   dcc <- doCohortCharacteristics(
     input.d = mtcars,
-    marker.name = "cyl",
+    marker.name = "vs",
     marker.description = "cylinders",
-    var.names = c("disp", "hp"),
-    var.descriptions = c("displacement", "horsepower"),
-    is.var.continuous = c(TRUE, TRUE),
+    var.names = c("disp", "hp", "am"),
+    var.descriptions = c("displacement", "horsepower", "transmission"),
+    is.var.continuous = c(TRUE, TRUE, FALSE),
     do.droplevels = TRUE,
-    caption = "Some mtcars summaries") 
+    caption = "Some mtcars summaries",
+    custom.total.label = "TOTAL",
+    custom.marker.labels = c("TOTAL", "Cylinders = 0", "Cylinders = 1"),
+    stat.tests = c("spearman", "kruskal", "wilcox", "chisq"))
   expect_length(dcc, 4)
   expect_is(dcc$result.table, "matrix")
+  expect_length(dcc$stat.tests.results, 3)
   expect_is(dcc$result.table.html, "character")
   expect_is(dcc$result.table.bamboo, "character")
+})
+
+test_that("statistical tests for categorical variables work", {
+  dcc_kendall <- doCohortCharacteristics(
+    input.d = mtcars, marker.name = "vs", marker.description = "cylinders",
+    var.names = c("am"), var.descriptions = c("transmission"),
+    is.var.continuous = c(FALSE), caption = "Some mtcars summaries",
+    stat.tests = "kendall")
+  dcc_chisq <- doCohortCharacteristics(
+    input.d = mtcars, marker.name = "vs", marker.description = "cylinders",
+    var.names = c("am"), var.descriptions = c("transmission"),
+    is.var.continuous = c(FALSE), caption = "Some mtcars summaries",
+    stat.tests = "chisq")
+  dcc_fisher <- doCohortCharacteristics(
+    input.d = mtcars, marker.name = "vs", marker.description = "cylinders",
+    var.names = c("am"), var.descriptions = c("transmission"),
+    is.var.continuous = c(FALSE), caption = "Some mtcars summaries",
+    stat.tests = "fisher")
+  dcc_cm_marker <- doCohortCharacteristics(
+    input.d = mtcars, marker.name = "vs", marker.description = "cylinders",
+    var.names = c("am"), var.descriptions = c("transmission"),
+    is.var.continuous = c(FALSE), caption = "Some mtcars summaries",
+    stat.tests = "confusionMarkerAsRef")
+  dcc_cm_var <- doCohortCharacteristics(
+    input.d = mtcars, marker.name = "vs", marker.description = "cylinders",
+    var.names = c("am"), var.descriptions = c("transmission"),
+    is.var.continuous = c(FALSE), caption = "Some mtcars summaries",
+    stat.tests = "confusionVarAsRef")
+  expect_length(dcc_kendall$stat.tests.results, 1)
+  expect_length(dcc_chisq$stat.tests.results, 1)
+  expect_length(dcc_fisher$stat.tests.results, 1)
+  expect_error(htmlTable(dcc_cm_marker$stat.tests.results), NA)
+  expect_error(htmlTable(dcc_cm_var$stat.tests.results), NA)
+})
+
+test_that("percentages can be for marginal rows or columns only", {
+  dcc_row <- doCohortCharacteristics(
+    input.d = mtcars,
+    marker.name = "wt",
+    marker.description = "weight",
+    var.names = c("disp", "hp", "am"),
+    var.descriptions = c("displacement", "horsepower", "transmission"),
+    is.var.continuous = c(TRUE, TRUE, FALSE),
+    show.percent = "row")
+  dcc_col <- doCohortCharacteristics(
+    input.d = mtcars,
+    marker.name = "wt",
+    marker.description = "weight",
+    var.names = c("disp", "hp", "am"),
+    var.descriptions = c("displacement", "horsepower", "transmission"),
+    is.var.continuous = c(TRUE, TRUE, FALSE),
+    show.percent = "column")
+  expect_error(dcc_row, NA)
+  expect_error(dcc_col, NA)
+})
+
+test_that("error when marker not found", {
+  expect_error(doCohortCharacteristics(mtcars, marker.name = "click",
+                                       var.names = c("A", "B")))
 })
