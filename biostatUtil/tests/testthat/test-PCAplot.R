1ebb0ec27048d061bc14840d08f7b7ddfe452fb4
Derek Chiu <dchiu@bccrc.ca>
2016-10-05 12:49:33 -0700

    cover single plot case of multiplot

--- biostatUtil/tests/testthat/test-PCAplot.R
+++ biostatUtil/tests/testthat/test-PCAplot.R
@@ -1,6 +1,13 @@
 
-context("Principal component scatterplots")
+context("Principal component scatterplots and multiple plotting")
+
+library(ggplot2)
 
 test_that("PCAplot works", {
   expect_error(PCAplot(mtcars$cyl, mtcars), NA)
+})
+
+test_that("One plot can still use multiplot", {
+  p <- ggplot(mtcars, aes(wt, mpg)) + geom_point()
+  expect_error(multiplot(p), NA)
 })
