805b8dc60b3f65457644175b993ba66bd685f03b
Derek Chiu <dchiu@bccrc.ca>
2016-12-14 14:11:53 -0800

    plotting funs boxplot and mean_var for MS data

---
+++ biostatUtil/R/ms_plot.R
@@ -0,0 +1,39 @@
+#' Boxplot of different MS expression data values
+#' 
+#' Generate boxplot for raw, log2, and vsn data values
+#' 
+#' @param x data object returned by \code{ms_process}
+#' @param path path to save boxplot to. Device is pdf.
+#' @param width width of plot
+#' @param height height of plot
+#' @return A pdf with three boxplots is saved to disk location specified by
+#'   \code{path}: the raw data values and the log2 and vsn transformed values.
+#' @author Derek Chiu
+#' @export
+ms_boxplot <- function(x, path, width = 8, height = 10) {
+  title <- "Raw data values"
+  pdf(file = path, width = width, height = height, useDingbats = FALSE)
+  Map(boxplot, x = dat[-1], main = c(title, paste0("log2(", title, ")"),
+                                     paste0("vsn(", title, ")")))
+  dev.off()
+}
+
+#' Mean-Variance Relationship
+#' 
+#' Generate mean-sd plots for the VSN data values.
+#' 
+#' @inheritParams ms_boxplot
+#' @param g vector of treatment groups
+#' @param title vector of titles for each treatment group
+#' @return A pdf is saved to disk location specified by \code{path}: the vsn
+#'   transformed values and mean-sd plots for each treatment group.
+#' @author Derek Chiu
+#' @export
+ms_mean_var <- function(x, g, title, path, width = 8, height = 10) {
+  pdf(file = path, width = width, height = height, useDingbats = FALSE)
+  boxplot(x$vsn, main = "vsn(Raw data values)")
+  capture.output(Map(function(g, t)
+    vsn::meanSdPlot(x$vsn[, grep(g, colnames(x$vsn))], plot = FALSE)$gg +
+      ggtitle(paste("vsn", t)), g = g, t = title))
+  dev.off()
+}
