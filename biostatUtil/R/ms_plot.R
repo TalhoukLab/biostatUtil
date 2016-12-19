ba86a3a20e4c3ec7684c007dd19192dcf0f8608a
Derek Chiu <dchiu@bccrc.ca>
2016-12-19 12:41:07 -0800

    use ggplot version for ms_boxplot

--- biostatUtil/R/ms_plot.R
+++ biostatUtil/R/ms_plot.R
@@ -6,28 +6,41 @@
 #' @param path path to save boxplot to. Device is pdf.
 #' @param width width of plot
 #' @param height height of plot
-#' @param las angle of axis labels
 #' @return A pdf with three boxplots is saved to disk location specified by
 #'   \code{path}: the raw data values and the log2 and vsn transformed values.
 #' @name ms_plot
 #' @family Mass Spectrometry
 #' @author Derek Chiu
 #' @export
-ms_boxplot <- function(x, path, width = 8, height = 10, las = 0) {
-  title <- "Raw data values"
-  pdf(file = path, width = width, height = height, useDingbats = FALSE)
-  par(las = las)
-  Map(boxplot, x = x[-1], main = c(title, paste0("log2(", title, ")"),
-                                   paste0("vsn(", title, ")")))
-  dev.off()
-  par(las = 0)
+ms_boxplot <- function(x, path, width = 8, height = 10) {
+  dat.plot <- lapply(x[c("raw", "l2", "vsn")], function(y)
+    gather(as.data.frame(y), key = Sample, value = Expression))
+  all.plots <- Map(ms_gg_boxplot, dat.plot,
+                   c("Raw data values",
+                     "log2(Raw data values)",
+                     "vsn(Raw data values)"))
+  plot <- gridExtra::marrangeGrob(all.plots, nrow = 1, ncol = 1)
+  ggsave(filename = path, plot = plot, width = width, height = height)
+}
+
+#' ggplot boxplot applied to each data source
+#' @noRd
+ms_gg_boxplot <- function(x, title) {
+  p <- ggplot(x, aes(x = Sample, y = Expression)) +
+    stat_boxplot(geom = "errorbar", width = 0.4) +
+    geom_boxplot() +
+    theme_linedraw() + 
+    theme(panel.grid = element_blank(),
+          plot.title = element_text(face = "bold")) +
+    ggtitle(title)
+  return(p)
 }
 
 #' Mean-Variance Relationship
 #' 
 #' Generate mean-sd plots for the VSN data values.
 #' 
-#' @inheritParams ms_boxplot
+#' @inheritParams ms_plot
 #' @param g vector of treatment groups
 #' @param title vector of titles for each treatment group
 #' @return A pdf is saved to disk location specified by \code{path}: the vsn
