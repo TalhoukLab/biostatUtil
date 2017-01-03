56b2668b3f214405deffb16c5a3299e93f43300b
Derek Chiu <dchiu@bccrc.ca>
2017-01-03 10:26:27 -0800

    resolve merged code

--- biostatUtil/tests/testthat/test-multiClassCM.R
+++ biostatUtil/tests/testthat/test-multiClassCM.R
@@ -21,16 +21,14 @@ test_that("Same results obtained whether fed a character, a numeric or a factor"
     multiClassCM(as.factor(x), as.factor(y))$table)
 })
 
-<<<<<<< HEAD
-test_that("Results are equivalent to Caret",{
-  (p <- sample(1:2, 100, replace=TRUE, prob=c(0.15, 0.25)))
-  (q <- sample(1:2, 100, replace=TRUE, prob=c(0.05, 0.4)))
-  a <- multiClassCM(p,q)
-  b <- caret::confusionMatrix(p,q)
-  })
-=======
+test_that("Results are equivalent to Caret", {
+  (p <- sample(1:2, 100, replace = TRUE, prob = c(0.15, 0.25)))
+  (q <- sample(1:2, 100, replace = TRUE, prob = c(0.05, 0.4)))
+  a <- multiClassCM(p, q)
+  b <- caret::confusionMatrix(p, q)
+})
+
 test_that("Error generated if uniques of x and y are not equivalent", {
   z <- sample(letters[1:k], 100, replace = TRUE, prob = c(0.05, 0.4, 0.65))
   expect_error(multiClassCM(y, z))
 })
->>>>>>> 4827890f8af9d33af02f75ea3615ecc048f051e3
