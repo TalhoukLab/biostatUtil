27dff40fedd4d68b0450638ba45fcb17f5bec938
Derek Chiu <dchiu@bccrc.ca>
2017-01-05 12:00:44 -0800

    better error message handling, simplify control flows

--- biostatUtil/R/findCutpointByCoxph.R
+++ biostatUtil/R/findCutpointByCoxph.R
@@ -14,41 +14,32 @@
 #' @section Warning: \code{surv.formula} cannot be multivariable. For example,
 #'   \code{Surv(time, status) ~ x + age} won't work but \code{Surv(time, status)
 #'   ~ x} is fine.
-#' @author Samuel Leung
+#' @author Samuel Leung, Derek Chiu
 #' @export
-findCutpointByCoxph <- function(input.d, surv.formula){
+findCutpointByCoxph <- function(input.d, surv.formula) {
   if (length(surv.formula[[3]]) > 1) {
-    print(paste("ERROR (find.cut.point.by.coxph): unable to process the survival formula: ", deparse(surv.formula)))
-    print("NOTE: formula must be of form: Surv(time,status) ~ variable")
-    return(NA)
+    stop("formula must be univariable: Surv(time, status) ~ variable.")
   }
 
   var.name <- deparse(surv.formula[[3]])
   if (!is.numeric(input.d[, var.name])) {
-    print(paste("ERROR (find.cut.point.by.coxph): variable (", var.name, ") is NOT NUMERIC!!!"))
-    return(NA)
+    stop(paste(var.name, "is not numeric."))
   }
   
   # find the min,max value of the variable
-  possible.values <- as.numeric(names(table(input.d[, var.name])))
-  num.possible.values <- length(possible.values)
-  if (num.possible.values < 2) {
-    print(paste("ERROR (find.cut.point.by.coxph): variable (", var.name, ") do not have any variation (i.e. all same value) for cut point determination."))
-    return(NA)
+  possible.values <- sort(unique(input.d[, var.name]))
+  if (length(possible.values) < 2) {
+    stop(paste0("variable '", var.name, "' does not have any variation (i.e. all values equal) for cutpoint assessment."))
   }
   
   # try all possible cut point
   cut.points <- aics <- hrs <- c()
-  for (cut.point in possible.values[2:length(possible.values)]) {
+  for (cut.point in possible.values[-1]) {
     cox.model.fit <- NA
     tryCatch({
       cox.model.fit <- coxph(as.formula(paste(deparse(surv.formula), ">=", cut.point)), data = input.d)
-    }, warning = function(w) {
-      # just ignore silently
     }, error = function(e) {
       print(paste("ERROR (find.cut.point.by.coxph) unexpected error occured:", e))
-    }, finally = function(e) {
-      # just ingore silently
     })
     if (length(cox.model.fit) > 1) { # cox.model.fit must not be NA, otherwise, length(cox.model.fit) would be 1
       # cox model with no warning/error
