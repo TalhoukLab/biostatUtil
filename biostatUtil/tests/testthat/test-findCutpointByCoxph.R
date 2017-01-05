72da733e9cde86320bf082520dfa0f181e94d35b
Derek Chiu <dchiu@bccrc.ca>
2017-01-05 12:00:54 -0800

    unit testing for findCutpointByCoxph

---
+++ biostatUtil/tests/testthat/test-findCutpointByCoxph.R
@@ -0,0 +1,23 @@
+
+context("Find cutpoint by coxph")
+
+data(lung)
+
+test_that("findCutpointByCoxph works for univariable case", {
+  obj <- findCutpointByCoxph(lung, Surv(time, status) ~ age)
+  expect_length(obj, 5)
+})
+
+test_that("error if formula is multivariable", {
+  expect_error(findCutpointByCoxph(lung, Surv(time, status) ~ age + sex))
+})
+
+test_that("error if explanatory variable is not continuous", {
+  lung$wt.loss <- as.character(lung$wt.loss)
+  expect_error(findCutpointByCoxph(lung, Surv(time, status) ~ wt.loss))
+})
+
+test_that("error if no variation in explanatory variable", {
+  lung$dummy <- 1
+  expect_error(findCutpointByCoxph(lung, Surv(time, status) ~ dummy))
+})
