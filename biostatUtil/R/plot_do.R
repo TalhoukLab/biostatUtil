7e86768399cd76401fea9f9f9ecf8aab897ce793
Derek Chiu <dchiu@bccrc.ca>
2017-01-04 16:15:11 -0800

    examples and unit tests for plot_do

--- biostatUtil/R/plot_do.R
+++ biostatUtil/R/plot_do.R
@@ -36,11 +36,12 @@ doBarplot <- function(data, var, xlab = var, ylab = "Frequency", title = NULL,
 
 #' Do a boxplot among subtypes
 #' 
-#' A stripchart (jitter plot) of a biomarker and some categorical subtype
-#' is superimposed on top of the boxplot
+#' A stripchart (jitter plot) of a biomarker and some categorical subtype is
+#' superimposed on top of the boxplot
 #' @param input.d input data.frame
 #' @param data.description boxplot title description
-#' @param biomarker.var.name biomarker variable name in \code{input.d} to graph on
+#' @param biomarker.var.name biomarker variable name in \code{input.d} to graph
+#'   on
 #' @param biomarker.name boxplot y-axis label
 #' @param subtype.var.name subtype variable name in \code{input.d} to graph on
 #' @param subtype.name boxplot x-axis label
@@ -49,10 +50,13 @@ doBarplot <- function(data, var, xlab = var, ylab = "Frequency", title = NULL,
 #' @param digits number of digits to round to
 #' @param ... additional arguments to \code{boxplot}
 #' @return a boxplot of biomarker values across different subtypes
-#' @note Expects subtype variable to be a factor. Also expects biomarker
-#' and subtype variable missing values to be \code{NA}
+#' @note Expects subtype variable to be a factor. Also expects biomarker and
+#'   subtype variable missing values to be \code{NA}
 #' @author Samuel Leung
 #' @export
+#' @examples
+#' doBoxplotAmongSubtypes(mtcars, "Boxplot of qsec vs. gear", "qsec", "QSEC",
+#' "gear", "GEAR")
 doBoxplotAmongSubtypes <- function(input.d, data.description,
                                    biomarker.var.name, biomarker.name,
                                    subtype.var.name, subtype.name, pch = 4,
@@ -151,6 +155,9 @@ doHist <- function(data, var, xlab = var, title = NULL, show.title = TRUE,
 #' @return A jitterplot shown across different subtypes
 #' @author Samuel Leung
 #' @export
+#' @examples
+#' doJitterplotAmongSubtypes(mtcars, "Boxplot of qsec vs. vs", "qsec", "QSEC",
+#' "vs", "VS")
 doJitterplotAmongSubtypes <- function(input.d, data.description,
                                       biomarker.var.name, biomarker.name,
                                       subtype.var.name, subtype.name,
