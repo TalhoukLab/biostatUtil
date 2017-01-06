b0bbde1f96f11ff6adacef2ad2db9d4109e02c80
Derek Chiu <dchiu@bccrc.ca>
2017-01-05 16:29:48 -0800

    start unit testing for km plotting funs

---
+++ biostatUtil/tests/testthat/test-plotKM.R
@@ -0,0 +1,23 @@
+
+context("Plot KM")
+
+library(survival)
+data(lung)
+
+test_that("doKMPlots calls ggkm or plotKM", {
+  expect_error(doKMPlots(lung, "time", "status", "sex", "Sex", use.ggkm = FALSE), NA)
+  expect_error(doKMPlots(lung, "time", "status", "sex", "Sex", use.ggkm = TRUE), NA)
+})
+
+test_that("factor variable drops unused levels", {
+  lung$sex <- as.factor(lung$sex)
+  expect_error(doKMPlots(lung, "time", "status", "sex", "Sex"), NA)
+})
+
+test_that("other plotting options can be specified", {
+  expect_error(doKMPlots(lung, "time", "status", "sex", "Sex", use.ggkm = FALSE,
+                         grey.scale = TRUE, shading.colors = NULL,
+                         legend.pos = "top"), NA)
+  expect_error(doKMPlots(lung, "time", "status", "sex", "Sex", use.ggkm = TRUE,
+                         show.risk = FALSE), NA)
+})
