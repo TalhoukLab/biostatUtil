b6851bdd264a352fb5b3c8b54f6f5bdc29feeb13
Derek Chiu <dchiu@bccrc.ca>
2017-03-20 11:20:39 -0700

    helper function to calculate left margin for different label lengths, various tidy data improvements

--- biostatUtil/R/ggkm.R
+++ biostatUtil/R/ggkm.R
@@ -12,7 +12,7 @@
 #' @param marks logical; if \code{TRUE} (default), censoring marks are shown on
 #'   survival curves
 #' @param CI logical; if \code{TRUE} (default), confidence bands are drawn for
-#'   survival curves
+#'   survival curves the using cumulative hazard, or log(survival).
 #' @param line.pattern linetype for survival curves
 #' @param shading.colors vector of colours for each survival curve
 #' @param main plot title
@@ -61,15 +61,19 @@ ggkm <- function(sfit, sfit2 = NULL, table = TRUE, returns = TRUE,
                  use.firth = 1, subs = NULL, 
                  legend = FALSE, legend.xy = c("x" = 0.8, "y" = 0.88),
                  legend.direction = "horizontal",
-                 line.y.increment = 0.05, digits = 3, min.p.value = 0.0001, ...) {
+                 line.y.increment = 0.05, digits = 3, min.p.value = 0.0001,
+                 ...) {
   time <- surv <- lower <- upper <- n.censor <- n.risk <- NULL
-  times <- seq(0, max(sfit$time), by = timeby)
+  times <- seq.int(0, max(sfit$time), by = timeby)
+  s1 <- levels(summary(sfit)$strata)
+  s2 <- summary(sfit, censored = TRUE)$strata
+  s3 <- summary(sfit, times = times, extend = TRUE)$strata
   if (is.null(subs)) {
-    subs1 <- 1:length(levels(summary(sfit)$strata))
-    subs2 <- 1:length(summary(sfit, censored = TRUE)$strata)
-    subs3 <- 1:length(summary(sfit, times = times, extend = TRUE)$strata)
+    subs1 <- seq_along(s1)
+    subs2 <- seq_along(s2)
+    subs3 <- seq_along(s3)
   } else {
-    for (i in 1:length(subs)) {
+    for (i in seq_along(subs)) {
       if (i == 1)
         ssvar <- paste0("(?=.*\\b=", subs[i])
       if (i == length(subs))
@@ -79,90 +83,70 @@ ggkm <- function(sfit, sfit2 = NULL, table = TRUE, returns = TRUE,
       if (i == 1 & i == length(subs))
         ssvar <- paste0("(?=.*\\b=", subs[i], "\\b)")
     }
-    subs1 <- which(regexpr(ssvar, levels(summary(sfit)$strata),
-                           perl = TRUE) != -1)
-    subs2 <- which(regexpr(ssvar, summary(sfit, censored = TRUE)$strata,
-                           perl = TRUE) != -1)
-    subs3 <- which(regexpr(ssvar, summary(sfit, times = times,
-                                          extend = TRUE)$strata,
-                           perl = TRUE) != -1)
+    subs1 <- which(regexpr(ssvar, s1, perl = TRUE) != -1)
+    subs2 <- which(regexpr(ssvar, s2, perl = TRUE) != -1)
+    subs3 <- which(regexpr(ssvar, s3, perl = TRUE) != -1)
   }
+  if (!is.null(cox.ref.grp))
+    names(cox.ref.grp) <- all.vars(sfit$call)[3]  # works for two-sided formulas
   if (!is.null(subs))
     pval <- FALSE
   if (is.null(ystratalabs))
     ystratalabs <- gsub(".*=(.)", "\\1", names(sfit$strata))
-  m <- max(nchar(ystratalabs))
-  times <- seq(0, max(sfit$time), by = timeby)
-  if (!is.null(cox.ref.grp))
-    names(cox.ref.grp) <- all.vars(sfit$call)[3]  # works for two-sided formulas
+  mleft <- left_margin(ystratalabs)  # left margins for km plot and risk table
   
-  .df <- data.frame(                      # data to be used in the survival plot
-    time = sfit$time[subs2],
-    n.risk = sfit$n.risk[subs2],
-    n.event = sfit$n.event[subs2],
-    n.censor = sfit$n.censor[subs2],
-    surv = sfit$surv[subs2],
-    strata = factor(summary(sfit, censored = TRUE)$strata[subs2]),
-    upper = sfit$upper[subs2],
-    lower = sfit$lower[subs2])
-  levels(.df$strata) <- ystratalabs       # final changes to data for survival plot
-  zeros <- data.frame(
-    time = 0, surv = 1,
-    strata = factor(ystratalabs, levels = levels(.df$strata)),
-    upper = 1, lower = 1)
-  .df <- plyr::rbind.fill(zeros, .df)
+  .df <- sfit %>%   # data to be used in the survival plot
+    broom::tidy() %>% 
+    select(time, n.risk, n.event, n.censor, surv = estimate,
+           strata, upper = conf.high, lower = conf.low) %>% 
+    mutate(strata = factor(s2, labels = ystratalabs))
+  zeros <- data.frame(time = 0, surv = 1,
+                      strata = factor(ystratalabs, levels = levels(.df$strata)),
+                      upper = 1, lower = 1)
+  .df <- bind_rows(zeros, .df)
   d <- length(levels(.df$strata))
   
-  # specifying plot parameteres etc ----
+  # Specifying plot parameteres etc
   names(shading.colors) <- ystratalabs
-  colScale <- scale_colour_manual(values = shading.colors)
-  colFill <- scale_fill_manual(values = shading.colors)
-  if (is.null(line.pattern) | length(line.pattern) == 1) {
-    line.pattern <- rep(1,length(ystratalabs))
-  }
-  names(line.pattern) <- ystratalabs
-  lineType <- scale_linetype_manual(values = line.pattern)
-  p <- ggplot(.df , aes(time, surv, color = strata, fill = strata)) +
-    geom_step(aes(color = strata,  linetype = strata), size = .7) + 
-    theme_bw() +
-    colScale +
-    colFill + 
-    lineType +
-    theme(axis.title.x = element_text(vjust = 0.5)) + 
+  if (is.null(line.pattern) | length(line.pattern) == 1)
+    line.pattern <- setNames(rep(1, d), ystratalabs)
+  p <- ggplot(.df , aes(time, surv, color = strata, fill = strata,
+                        linetype = strata)) +
+    geom_step(size = .7) + 
+    scale_colour_manual(values = shading.colors) +
+    scale_fill_manual(values = shading.colors) + 
+    scale_linetype_manual(values = line.pattern) +
     scale_x_continuous(xlabs, breaks = times, limits = xlims) + 
     scale_y_continuous(ylabs, limits = ylims) + 
-    theme(panel.background = element_blank(),
-          panel.grid.minor = element_blank(),
-          panel.grid.major = element_blank()) +
-    theme(plot.margin = grid::unit(c(0, 1, .5,
-                                     ifelse(m < 10, 1.5, 2.5)), "lines")) +
+    theme_bw() +
+    theme(axis.title.x = element_text(vjust = 0.5),
+          panel.background = element_blank(),
+          panel.grid = element_blank(),
+          plot.margin = grid::unit(c(0, 1, .5, mleft$margin.km),
+                                   c("lines", "lines", "lines", "in"),
+                                   list(NULL, NULL, NULL, NULL))) +
     ggtitle(main)
-  if (legend == TRUE)  # Legend----
-  p <- p + theme(legend.position = legend.xy[c("x","y")]) +
+  if (legend == TRUE)  # Legend
+  p <- p + theme(legend.position = legend.xy[c("x", "y")]) +
     theme(legend.key = element_rect(colour = NA)) +
     theme(legend.title = element_blank()) +
     theme(legend.direction = "horizontal")
   else
     p <- p + theme(legend.position = "none")
-  if (CI == TRUE)  # Confidence Bands----  
+  if (CI == TRUE)  # Confidence Bands
   p <- p + geom_ribbon(data = .df, aes(ymin = lower, ymax = upper),
                        alpha = 0.05, linetype = 0) 
-  if (marks == TRUE)  # Censor Marks----
+  if (marks == TRUE)  # Censor Marks
   p <- p + geom_point(data = subset(.df, n.censor >= 1), 
                       aes(x = time, y = surv), shape = "/", size = 4)
-  ## Create a blank plot for place-holding
-  blank.pic <- ggplot(.df, aes(time, surv)) + geom_blank() + theme_bw() +
-    theme(axis.text.x = element_blank(), axis.text.y = element_blank(),
-          axis.title.x = element_blank(), axis.title.y = element_blank(),
-          axis.ticks = element_blank(), panel.grid.major = element_blank(),
-          panel.border = element_blank())
-  # p-value placement----
+  # p-value placement (depends on whether there is a sfit2)
   if (is.null(sfit2)) {
     if (pval) {
       sdiff <- survdiff(eval(sfit$call$formula), data = eval(sfit$call$data))
-      pval <- pchisq(sdiff$chisq,length(sdiff$n) - 1, lower.tail = FALSE)
+      pval <- pchisq(sdiff$chisq, length(sdiff$n) - 1, lower.tail = FALSE)
       pvaltxt <- ifelse(pval < min.p.value,
-                        paste("Log Rank p <", format(min.p.value, scientific = FALSE)),
+                        paste("Log Rank p <", format(min.p.value,
+                                                     scientific = FALSE)),
                         paste("Log Rank p =", signif(pval, digits)))
       if (HR) {
         pretty.coxph.obj <- prettyCoxph(eval(sfit$call$formula),
@@ -178,9 +162,10 @@ ggkm <- function(sfit, sfit2 = NULL, table = TRUE, returns = TRUE,
         show.ref.group <- length(HRtxts) > 1
         cox.strata.labs <- ystratalabs
         if (!is.null(cox.ref.grp)) {
-          cox.strata.labs <- c(cox.ref.grp,ystratalabs[ystratalabs != cox.ref.grp])
+          cox.strata.labs <- c(cox.ref.grp,
+                               ystratalabs[ystratalabs != cox.ref.grp])
         }
-        for (i in 1:length(HRtxts)) {
+        for (i in seq_along(HRtxts)) {
           HRtxt <- HRtxts[i]
           if (show.ref.group) {
             HRtxt <- paste0(HRtxt, " ~ ", cox.strata.labs[i + 1],
@@ -199,7 +184,8 @@ ggkm <- function(sfit, sfit2 = NULL, table = TRUE, returns = TRUE,
       sdiff <- survdiff(eval(sfit2$call$formula), data = eval(sfit2$call$data))
       pval <- pchisq(sdiff$chisq, length(sdiff$n) - 1, lower.tail = FALSE)
       pvaltxt <- ifelse(pval < min.p.value,
-                        paste("Log Rank p <", format(min.p.value, scientific = FALSE)),
+                        paste("Log Rank p <", format(min.p.value,
+                                                     scientific = FALSE)),
                         paste("Log Rank p =", signif(pval, digits)))
       if (HR) {
         pretty.coxph.obj <- prettyCoxph(eval(sfit2$call$formula),
@@ -215,9 +201,10 @@ ggkm <- function(sfit, sfit2 = NULL, table = TRUE, returns = TRUE,
         show.ref.group <- length(HRtxts) > 1
         cox.strata.labs <- ystratalabs
         if (!is.null(cox.ref.grp)) {
-          cox.strata.labs <- c(cox.ref.grp,ystratalabs[ystratalabs != cox.ref.grp])
+          cox.strata.labs <- c(cox.ref.grp,
+                               ystratalabs[ystratalabs != cox.ref.grp])
         }
-        for (i in 1:length(HRtxts)) {
+        for (i in seq_along(HRtxts)) {
           HRtxt <- HRtxts[i]
           if (show.ref.group) {
             HRtxt <- paste0(HRtxt, " ~ ", cox.strata.labs[i + 1],
@@ -229,53 +216,58 @@ ggkm <- function(sfit, sfit2 = NULL, table = TRUE, returns = TRUE,
         }
       }
     }
-    p <- p + annotate("text", x = 0.2 * max(sfit2$time), y = 0.01,
+    p <- p + annotate("text", x = 0.2 * max(sfit2$time), hjust = 0, y = 0.01,
                       label = ifelse(pval, pvaltxt, ""), size = 3)
   }
   
-  # Create table graphic to include at-risk numbers----
+  # Create table graphic to include at-risk numbers, keep at-risk numbers 
+  # same order as appears in HR (do not reverse levels)
   if (table) {
-    risk.data <- data.frame(
-      strata = factor(summary(sfit,times = times,
-                              extend = TRUE)$strata[subs3], ),
-      time = as.numeric(summary(sfit,times = times,
-                                extend = TRUE)$time[subs3]) * 0.97 +
-        xlims[2] * 0.02, # doing some nasty empirical scaling/shifting here!!!  remove scaling/shifting factors if dosen't look right
-      n.risk = summary(sfit, times = times, extend = TRUE)$n.risk[subs3]
-    )
-    risk.data$strata <- factor(risk.data$strata,
-                               # want to print at-risk numbers same order as appears in HR, 
-                               # therefore, do not rev the levels	
-                               levels = levels(risk.data$strata))#levels = rev(levels(risk.data$strata)))
+    risk.data <- sfit %>% 
+      summary(times = times, extend = TRUE) %>% 
+      list() %>% 
+      purrr::map_df(`[`, c("strata", "time", "n.risk"))
     data.table <- ggplot(risk.data, aes(x = time, y = strata,
                                         label = format(n.risk, nsmall = 0))) +
-      geom_text(size = 3.5) + theme_bw() +
-      scale_y_discrete(breaks = as.character(levels(risk.data$strata)),
-                       # want to print at-risk numbers same order as appears in HR, 
-                       # therefore, do not rev the levels		  
-                       labels = ystratalabs) +#labels = rev(ystratalabs)) +
+      geom_text(size = 3.5) +
+      scale_y_discrete(labels = ystratalabs) +
       scale_x_continuous("Numbers at risk", limits = xlims) +
-      theme(axis.title.x = element_text(size = 12, vjust = 1),
-            panel.grid.major = element_blank(),
-            panel.grid.minor = element_blank(),
+      theme_bw() +
+      theme(panel.grid = element_blank(),
             panel.border = element_blank(),
-            axis.text.x = element_blank(),
             axis.ticks = element_blank(),
-            axis.text.y = element_text(face = "bold",
-                                       # want to print at-risk numbers same order as appears in HR, 
-                                       # therefore, do not rev the levels					
-                                       color = shading.colors[1:length(ystratalabs)],#color = rev(shading.colors[1:length(ystratalabs)]),
-                                       hjust = 1),
+            axis.text.x = element_blank(),
+            axis.text.y = element_text(color = shading.colors[ystratalabs],
+                                       face = "bold", hjust = 1),
+            axis.title.x = element_text(size = 12, vjust = 1),
             legend.position = "none",
-            plot.margin = grid::unit(
-              c(-1.5, 1, 0.1, ifelse(m < 10, 2.5, 3.5) - 0.28 * m), "lines")) +
-      xlab(NULL) + ylab(NULL)
+            plot.margin = grid::unit(c(0.5, 1.25, 0.5, mleft$margin.rt),
+                                     "lines")) +
+      labs(y = NULL)
     if (returns)
-      gridExtra::grid.arrange(p, blank.pic, data.table,
-                              clip = FALSE, nrow = 3, ncol = 1,
-                              heights = grid::unit(c(2, .1, .25),
-                                                   c("null", "null", "null")))
-  } else {
+      gridExtra::grid.arrange(p, data.table, clip = FALSE, nrow = 2, ncol = 1,
+                              heights = grid::unit(c(2, .5), "null"))
+   } else {
     return(p)
   }
+}
+
+#' Get left margin distances for km plot and risk table
+left_margin <- function(labels) {
+  max.nc <- max(nchar(labels))
+  if (max.nc <= 4) {
+    mleft.km <- 0
+    mleft.rt <- 2.4
+  } else {
+    mleft.km <- graphics::strwidth(labels[which.max(nchar(labels))],
+                                   units = "in")
+    if (max.nc <= 5) {
+      mleft.km <- mleft.km - (max.nc / 100 + 0.4)
+    } else {
+      mleft.km <- mleft.km - (max.nc / 100 + 0.5)
+    }
+    mleft.rt <- 0.5
+  }
+  return(list(margin.km = mleft.km,
+              margin.rt = mleft.rt))
 }
