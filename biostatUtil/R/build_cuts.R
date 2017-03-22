fc7bcd6623de9231dbde834df8f75db2fa8494ed
Derek Chiu <dchiu@bccrc.ca>
2017-03-22 14:10:05 -0700

    use tibble output, purrr machinery

--- biostatUtil/R/build_cuts.R
+++ biostatUtil/R/build_cuts.R
@@ -1,44 +1,43 @@
 #' Build cutpoint variables
 #' 
-#' Transforms an ordinal variable into anywhere from two to five groups for cutpoint analysis.
-#'
+#' Transforms an ordinal variable into anywhere from two to five groups for
+#' cutpoint analysis.
+#' 
 #' @param x vector to split by certain cutpoints
-#' @param n either "b" for binarization (2 groups), "t" for trinarization (3 groups),
-#' "q" for quads (4 groups), or "qn" for quints (5 groups)
+#' @param n either "b" for binarization (2 groups), "t" for trinarization (3
+#'   groups), "q" for quads (4 groups), or "qn" for quints (5 groups)
 #' @param var.prefix variable name prefix
 #' @param list if \code{TRUE}, the variables are returned as a list.
-#'
-#' @return By default, a data frame of cutpoint variables built from a categorical biomarker. The
-#' number of columns correspond to all the ways the biomarker could be cut into \code{n}
-#' bins. Each column name starts with a "b", "t", "qd", or "qn" for "binarization", "trinarization",
-#' "quads", or "quints", respectively, with the levels being compared separated by "v". If \code{list = FALSE},
-#' each cutpoint variable is an element of a list.
-#' 
+#'   
+#' @return By default, a tibble of cutpoint variables built from a categorical
+#'   biomarker. The number of columns correspond to all the ways the biomarker
+#'   could be cut into \code{n} bins. Each column name starts with a "b", "t",
+#'   "qd", or "qn" for "binarization", "trinarization", "quads", or "quints",
+#'   respectively, with the levels being compared separated by "v". If 
+#'   \code{list = FALSE}, each cutpoint variable is an element of a list.
+#'   
 #' @author Derek Chiu
 #' @export
-#'
+#' 
 #' @examples
 #' set.seed(1108)
 #' x <- sample(0:4, size = 1000, replace = TRUE)
-#' head(build_cuts(x, n = "b"))
-#' head(build_cuts(x, n = "t"))
-#' head(build_cuts(x, n = "t", var.prefix = "PHGDH"))
+#' build_cuts(x, n = "b")
+#' build_cuts(x, n = "t")
+#' build_cuts(x, n = "t", var.prefix = "PHGDH")
 #' str(build_cuts(x, n = "qd", list = TRUE))
 build_cuts <- function(x, n = c("b", "t", "qd", "qn"), var.prefix = NULL,
                        list = FALSE) {
   . <- NULL
-  ulevs <- sort(unique(x[x > min(x,na.rm=TRUE)]))
+  ulevs <- sort(unique(x[x > min(x, na.rm = TRUE)]))
   ng <- switch(match.arg(n), b = 2, t = 3, qd = 4, qn = 5)
   assertthat::assert_that(length(ulevs) >= ng - 1)
-  cuts <- rbind(combn(ulevs, ng - 1), max(x,na.rm=TRUE))
-  cut.list <- plyr::alply(cuts, 2, Hmisc::cut2, x = x)
-  res <- cut.list %>% 
-    as.data.frame() %>% 
-    magrittr::set_names(apply(cuts, 2, function(c) name_cuts(x, c))) %>% 
+  res <- combn(ulevs, ng - 1, simplify = FALSE) %>%
+    purrr::map(c, max(x, na.rm = TRUE)) %>% 
+    magrittr::set_names(purrr::map_chr(., name_cuts, x = x)) %>% 
+    purrr::map_df(Hmisc::cut2, x = x) %>% 
     magrittr::set_names(ifelse(rep(is.null(var.prefix), ncol(.)), names(.),
                                paste0(var.prefix, "_", names(.))))
-  if (list)
-    return(as.list(res))
-  else
-    return(res)
+  if (list) res <- as.list(res)
+  return(res)
 }
