471440e3e2c5325120e9aeb48d4990a563db43ec
Derek Chiu <dchiu@bccrc.ca>
2017-03-22 12:28:35 -0700

    add plot.ph param, improve code style in conditional chunks, simplify result obj

--- biostatUtil/R/prettyCoxph.R
+++ biostatUtil/R/prettyCoxph.R
@@ -2,23 +2,24 @@
 #' 
 #' Formats the coxph (or coxphf) model outputs into a prettier display.
 #' 
-#' The original fit call is returned, along with other useful statistics.
-#' If Firth is called, we first determine if the percentage of
-#' censored cases is large enough to use Firth.
+#' The original fit call is returned, along with other useful statistics. If
+#' Firth is called, we first determine if the percentage of censored cases is
+#' large enough to use Firth.
 #' 
-#' @param input.formula a formula object, with the response on the left of a
-#' \code{~} operator, and the terms on the right. The response must be a
-#' survival object as returned by the \code{Surv} function.
+#' @param input.formula a formula object, with the response on the left of a 
+#'   \code{~} operator, and the terms on the right. The response must be a 
+#'   survival object as returned by the \code{Surv} function.
 #' @param input.d data.frame containing the variables in \code{input.formula}
 #' @param ref.grp a named array indicating reference group(s) for any variables 
-#' in the cox model.  this parameter will be ignored if not applicable, e.g. for 
-#' continuous variable
-#' @param use.firth if set to -1, Firth's correction is applied using
+#'   in the cox model.  this parameter will be ignored if not applicable, e.g.
+#'   for continuous variable
+#' @param use.firth if set to -1, Firth's correction is applied using 
 #' \code{coxphf}. The default value is 1, which uses \code{coxph}.
 #' @param check.ph if \code{TRUE}, checks the proportional hazard assumption.
-#' @param ph.test.plot.filename filename of residual plot. If set to
-#' \code{no.file} (default), plots to console. If \code{NA}, no plot will be
-#' generated.
+#' @param plot.ph if \code{TRUE} (default), the PH residual plot is graphed.
+#' @param ph.test.plot.filename character string for filename of residual plot,
+#'   including file extension. If \code{NULL} (default), plots to console.
+#'   Otherwise plot is saved to disk.
 #' @param ... additional arguments to \code{coxph} or \code{coxphf}
 #' @return A list with elements
 #' \item{output}{A character matrix showing the hazard ratio, confidence bounds,
@@ -53,9 +54,12 @@
 #' # Releveled reference group
 #' prettyCoxph(Surv(time, status) ~ x + strata(sex), test1, ref.grp =
 #' setNames("2", "x"))
+#' 
+#' # Use Firth's correction
+#' prettyCoxph(Surv(time, status) ~ x + strata(sex), test1, use.firth = -1)
 prettyCoxph <- function(input.formula, input.d, ref.grp = NULL, use.firth = 1,
-                        check.ph = FALSE,
-                        ph.test.plot.filename = "no.file", ...) {
+                        check.ph = FALSE, plot.ph = TRUE,
+                        ph.test.plot.filename = NULL,...) {
   pos <- 1
   assign(".my.formula", as.formula(
     paste0("survival::", paste(deparse(input.formula), collapse = ""))),
@@ -75,7 +79,7 @@ prettyCoxph <- function(input.formula, input.d, ref.grp = NULL, use.firth = 1,
   ok.to.use.firth <- ifelse(use.firth == -1, TRUE, FALSE)
   if (use.firth < 1 & use.firth > -1) {
     for (var.name in all.vars(.my.formula)[-c(1, 2)]) {
-      if (is.factor(.my.data[, var.name])) {
+      if (is.factor(.my.data[[var.name]])) {
         fit <- survfit(as.formula(paste(deparse(.my.formula[[2]]),
                                         "~", var.name)), data = .my.data)
         for (i in seq_len(nrow(fit))) {
@@ -91,58 +95,51 @@ prettyCoxph <- function(input.formula, input.d, ref.grp = NULL, use.firth = 1,
   }
   fit <- coxph(.my.formula, .my.data, ...)
   .my.formula <- fit$formula
-  ph.check <- "NOT CALCULATED"
   
   if (ok.to.use.firth) {
-    .my.data <- .my.data[apply(.my.data[, all.vars(.my.formula)], 1,
-                               function(x) !any(is.na(x))), ]
+    .my.data <- .my.data %>% 
+      as.data.frame() %>% 
+      magrittr::extract(apply(.[all.vars(.my.formula)], 1,
+                              function(x) !any(is.na(x))), )
     fit.firth <- coxphf::coxphf(.my.formula, .my.data, ...)
     fit.firth$nevent <- sum(fit.firth$y[, "status"])
-    # if lower/upper ci becomes NaN, change to NA for better display 
-    # e.g. in KM plot "NA" would be easier to understand compared to "NaN"
     fit.firth$ci.lower[is.nan(fit.firth$ci.lower)] <- NA
     fit.firth$ci.upper[is.nan(fit.firth$ci.upper)] <- NA
+    # If becomes NaN, change to NA for better display/understanding in KM plot
   } else {
     fit.firth <- NA
   }
   
   if (check.ph) {
     ph.test <- cox.zph(fit)
-    ph.check <- cbind("PH test" = ph.test$table[rownames(ph.test$table) !=
-                                                  "GLOBAL", "p"])
-    if (!is.na(ph.test.plot.filename)) {
-      if (ph.test.plot.filename != "no.file") {
+    ph.check <- ph.test$table %>% 
+      magrittr::extract(rownames(.) != "GLOBAL", "p") %>% 
+      cbind("PH test" = .)
+    if (plot.ph) {
+      if (is.null(ph.test.plot.filename)) {
+        plot(ph.test)
+      } else {
         pdf(ph.test.plot.filename)
         plot(ph.test)
         dev.off()
-      } else {
-        plot(ph.test)
       }
     }
+  } else {
+    ph.test <- NA
+    ph.check <- "NOT CALCULATED"
   }
   
   if (ok.to.use.firth) {
     result <- cbind(exp(fit.firth$coefficients), fit.firth$ci.lower,
                     fit.firth$ci.upper, fit.firth$prob, ph.check)
   } else {
-    if (any(grepl("\\+", .my.formula)))
-      result <- cbind(summary(fit)$conf.int[, c('exp(coef)', 'lower .95',
-                                                'upper .95')],
-                      "Pr(>|z|)" = summary(fit)$coefficients[, 'Pr(>|z|)'],
-                      ph.check)
-    else
-      result <- cbind(rbind(summary(fit)$conf.int[, c('exp(coef)', 'lower .95',
-                                                      'upper .95')]),
-                      "Pr(>|z|)" = summary(fit)$coefficients[, 'Pr(>|z|)'],
-                      ph.check)
+    result <- cbind(summary(fit)$conf.int[, c("exp(coef)", "lower .95",
+                                              "upper .95")],
+                    "Pr(>|z|)" = summary(fit)$coefficients[, "Pr(>|z|)"],
+                    ph.check)
   }
-  if (check.ph)
-    return.obj <- list(output = result, fit = fit, fit.firth = fit.firth,
-                       n = fit$n, nevent = fit$nevent, ph.test = ph.test,
-                       used.firth = ok.to.use.firth)
-  else
-    return.obj <- list(output = result, fit = fit, fit.firth = fit.firth,
-                       n = fit$n, nevent = fit$nevent,
-                       used.firth = ok.to.use.firth)
+  return.obj <- list(output = result, fit = fit, fit.firth = fit.firth,
+                     n = fit$n, nevent = fit$nevent, ph.test = ph.test,
+                     used.firth = ok.to.use.firth)
   return(return.obj)
 }
