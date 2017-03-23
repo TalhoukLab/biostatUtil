0737f4ac17b9d0117e22e46e85833a7e3f8dce08
Derek Chiu <dchiu@bccrc.ca>
2017-03-23 14:55:05 -0700

    remove import

--- biostatUtil/R/bootMean.R
+++ biostatUtil/R/bootMean.R
@@ -34,7 +34,7 @@ bootMean <- function(x, num.boot = 1000, conf.level = 0.95, seed = 12, ...) {
   obs.mean <- mean(x, ...)
   ci <- replicate(num.boot, mean(sample(x, replace = TRUE), ...)) %>% 
     sort() %>% 
-    magrittr::extract(c(floor(num.boot * (1 - conf.level) / 2),
-                        ceiling(num.boot * (1 - (1 - conf.level) / 2))))
+    extract(c(floor(num.boot * (1 - conf.level) / 2),
+              ceiling(num.boot * (1 - (1 - conf.level) / 2))))
   return(list(obs.mean = obs.mean, ci = ci, n = length(x)))
 }
