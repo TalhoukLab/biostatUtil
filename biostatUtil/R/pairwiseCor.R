88a12b129208e09afe53e24be5069d1e2eb73aaa
Derek Chiu <dchiu@bccrc.ca>
2017-03-23 15:47:30 -0700

    use purrr in pairwiseCor, input is df

--- biostatUtil/R/pairwiseCor.R
+++ biostatUtil/R/pairwiseCor.R
@@ -2,7 +2,7 @@
 #' 
 #' Computes all pairwise correlations between the columns of a data frame
 #' 
-#' @param dataframe A data frame containing numeric variables of interest.
+#' @param x A data frame containing numeric variables of interest.
 #' @return all pairwise absolute correlations, correlations, Pval, Adj P val by 
 #'   decreasing order of absolute correlations.
 #'   
@@ -13,19 +13,17 @@
 #' set.seed(123)
 #' x <- data.frame(matrix(rnorm(25), nrow = 5))
 #' pairwiseCor(x)
-pairwiseCor <- function(dataframe) {
-  if (!all(apply(dataframe, 2, is.numeric))) {
+pairwiseCor <- function(x) {
+  if (!all(purrr::map_lgl(x, is.numeric)))
     stop("All columns of data matrix must be numeric")
-  }
   Cor <- AbsCor <- Pval <- AdjP <- NULL
-  x <- dataframe
-  pairs <- combn(colnames(dataframe), 2) %>% 
+  pairs <- combn(names(x), 2) %>% 
     set_rownames(paste0("Variable", 1:2))
   pairwiseCorDF <- data.frame(Cor = apply(pairs, 2, function(df)
     cor(x[, df]))[2, ]) %>% 
     mutate(AbsCor = abs(Cor),
-           Pval = mapply(function(v1, v2) cor.test(v1, v2)$p.value,
-                         x[pairs[1, ]], x[pairs[2, ]]),
+           Pval = purrr::map2_dbl(x[pairs[1, ]], x[pairs[2, ]],
+                                  ~ cor.test(.x, .y)$p.value),
            AdjP = p.adjust(Pval, "fdr")) %>% 
     select(AbsCor, Cor, Pval, AdjP) %>% 
     round(4) %>% 
