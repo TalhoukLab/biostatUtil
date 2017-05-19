624f7d1fc017df10b9f0cbd63f743500d72c5adb
Derek Chiu <dchiu@bccrc.ca>
2017-05-19 16:13:12 -0700

    don't need to attach ggplot2 package for short test

--- biostatUtil/tests/testthat/test-PCAplot.R
+++ biostatUtil/tests/testthat/test-PCAplot.R
@@ -1,13 +1,12 @@
 
 context("Principal component scatterplots and multiple plotting")
 
-suppressPackageStartupMessages(library(ggplot2))
-
 test_that("PCAplot works", {
   expect_error(PCAplot(mtcars$cyl, mtcars), NA)
 })
 
 test_that("One plot can still use multiplot", {
-  p <- ggplot(mtcars, aes(wt, mpg)) + geom_point()
+  p <- ggplot2::ggplot(mtcars, aes(wt, mpg)) +
+    ggplot2::geom_point()
   expect_error(multiplot(p), NA)
 })
