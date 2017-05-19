062e0e4c568bbc34a023c9a203ade5fba01fde33
Derek Chiu <dchiu@bccrc.ca>
2017-05-19 16:00:06 -0700

    suppress warnings in indepTests

--- biostatUtil/tests/testthat/test-indepTests.R
+++ biostatUtil/tests/testthat/test-indepTests.R
@@ -6,7 +6,7 @@ test_that("Output works with defaults", {
   set.seed(1108)
   A <- rbinom(100, 3, 0.2)
   B <- rbinom(100, 4, 0.8)
-  ct <- CrossTable(A, B, chisq = TRUE, fisher = TRUE)
+  ct <- CrossTable(A, B, prop.chisq = FALSE, chisq = FALSE, fisher = TRUE)
   expect_error(indepTests(ct), NA)
 })
 
@@ -14,8 +14,7 @@ test_that("Continuity Correction only when Pearson is not NA", {
   set.seed(1108)
   A <- rbinom(100, 1, 0.2)
   B <- rbinom(100, 1, 0.8)
-  ct1 <- CrossTable(A, B)
-  expect_error(indepTests(ct1), NA)
+  expect_warning(indepTests(CrossTable(A, B)))
   
   ct2 <- CrossTable(matrix(c(20, 25, 15, 30), nrow = 2))
   df <- indepTests(ct2)
