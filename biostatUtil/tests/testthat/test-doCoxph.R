58350f13cd01dc1c9089e65c9b9f988ebd73f05f
Derek Chiu <dchiu@bccrc.ca>
2017-07-26 11:05:38 -0700

    test interaction term case

--- biostatUtil/tests/testthat/test-doCoxph.R
+++ biostatUtil/tests/testthat/test-doCoxph.R
@@ -173,7 +173,7 @@ test_that("doCoxphMultivariable multiple survival outcomes works", {
 
 context("Cox model testing interaction")
 
-test_that("doInteractionCox works", {
+test_that("doInteractionCox works without an interaction term", {
   res <- doInteractionCox(input.d = lung,
                           var.names = c("sex", "ph.ecog"),
                           var.descriptions = c("Sex", "ECOG score"),
@@ -184,6 +184,21 @@ test_that("doInteractionCox works", {
                           var.ref.groups = c("2", "0"),
                           caption = "")
   expect_length(res, 4)
+})
+
+test_that("doInteractionCox works with an interaction term", {
+  lung[["ph.karno:pat.karno"]] <- lung$ph.karno * lung$pat.karno
+  lung$ph.karno <- factor(lung$ph.karno)
+  lung$pat.karno <- factor(lung$pat.karno)
+  res <- doInteractionCox(input.d = lung,
+                          var.names = "ph.karno:pat.karno",
+                          var.descriptions = "karnos",
+                          var.names.surv.time = c("time", "time2"),
+                          var.names.surv.status = c("status", "status2"),
+                          event.codes.surv = c("2", "2"),
+                          surv.descriptions = c("OS", "OS2"),
+                          var.ref.groups = c("2", "0"),
+                          caption = "")
 })
 
 test_that("doInteractionCox factors use lowest group if no reference specified", {
