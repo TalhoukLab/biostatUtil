92c532bb1f9895e26a4619439319a5952cb55089
Derek Chiu <dchiu@bccrc.ca>
2019-02-14 15:00:12 -0800

    don't load pkg to suppress R version warning

--- biostatUtil/tests/testthat/test-binaryCMAsHTML.R
+++ biostatUtil/tests/testthat/test-binaryCMAsHTML.R
@@ -1,7 +1,6 @@
 
 context("Binary class confusion matrices as HTML format")
 
-library(htmlTable)
 set.seed(547)
 n <- 20
 x <- rbinom(n, size = 1, prob = 0.6)
@@ -9,5 +8,5 @@ y <- rbinom(n, size = 1, prob = 0.4)
 results <- binaryCMAsHTML(x, y, "Test", num.boot = 1000)
 
 test_that("binaryCMAsHTML works", {
-  expect_error(htmlTable(results), NA)
+  expect_error(htmlTable::htmlTable(results), NA)
 })
