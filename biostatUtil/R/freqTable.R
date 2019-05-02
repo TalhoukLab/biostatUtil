87ca60a969d1649d777cfca2b94f396acb3407ad
Derek Chiu <dchiu@bccrc.ca>
2019-05-02 12:21:09 -0700

    dplyr::funs is soft deprecated as of v0.8.0

--- biostatUtil/R/freqTable.R
+++ biostatUtil/R/freqTable.R
@@ -51,14 +51,14 @@ freqTable <- function(x, levels = sort(unique(as.character(x))),
             sum(.$Frequency[!is.na(.$`Valid Percent`) & .$Class == "Valid"]),
             sum(.$Percent[!is.na(.$`Valid Percent`) & .$Class == "Valid"]), 100)) %>%
     arrange(Class, Score) %>%
-    mutate_each(funs(as.numeric), matches("Frequency|Percent")) %>%
+    mutate_each(list(as.numeric), matches("Frequency|Percent")) %>%
     mutate(Score = ifelse(Class == "Total" & Score == "Total", "",
                           ifelse(is.na(Score), "Total", as.character(Score))),
            `Valid Percent` = ifelse(Class == "Total", NA, `Valid Percent`),
            `Cumulative Percent` = ifelse(!is.na(`Valid Percent`) & Score != "Total",
                                          cumsum(Frequency) / max(Frequency[Class == "Valid"]) * 100, NA),
            Class = ifelse(duplicated(Class), "", as.character(Class))) %>%
-    mutate_each(funs(round(., 1)), contains("Percent"))
+    mutate_each(list(~round(., 1)), contains("Percent"))
   if (is.null(missing)) {
     tab <- tab %>%
       magrittr::extract(-which(.$Class == "Total"), ) %>%
