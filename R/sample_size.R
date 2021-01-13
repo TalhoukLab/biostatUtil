313a7daccae9c44ddf892c6fd1fb0efc017994ff
Derek <dchiu@bccrc.ca>
2021-01-13 15:24:55 -0800

    add helper to convert R2 to D, use defaults from royston() for ssize_D()

--- R/sample_size.R
+++ R/sample_size.R
@@ -5,9 +5,11 @@
 #' The sample size calculation uses a discrimination measured based on
 #' multivariable prognostic time-to-event models.
 #'
-#' @param D discrimination measure D, defaults to 1.4 without any guidance or
-#' prior models.
-#' @param cens censoring proportion, defaults to 10%.
+#' @param fit an object fit of class `coxph`
+#' @param D discrimination measure D, defaults to output of
+#'   `survival::royston()` for `fit`
+#' @param cens censoring proportion, defaults to proportion calculated from
+#'   `fit`
 #' @param p proportion of `D` accepted as `delta`, defaults to 10%.
 #' @param alpha Type I error
 #' @param power statistical power (1 - Type II error)
@@ -18,15 +20,19 @@
 #'   input parameters.
 #' @author Derek Chiu
 #' @references https://www.ncbi.nlm.nih.gov/pmc/articles/PMC4603804/
-ssize_D <- function(D = 1.4, cens = 0.1, p = 0.1, alpha = 0.05, power = 0.8,
-                    delta = 0.25, trial = c("superiority", "non-inferiority")) {
+ssize_D <- function(fit, D = NULL, cens = NULL, p = 0.1, alpha = 0.05,
+                    power = 0.8, delta = 0.25,
+                    trial = c("superiority", "non-inferiority")) {
   trial <- match.arg(trial)
   beta <- 1 - power
   zz <- switch(
     trial,
-    superiority = qnorm(1 - alpha / 2) + qnorm(1 - beta),
-    `non-inferiority` = qnorm(1 - alpha) + qnorm(1 - beta)
+    superiority = stats::qnorm(1 - alpha / 2) + stats::qnorm(1 - beta),
+    `non-inferiority` = stats::qnorm(1 - alpha) + stats::qnorm(1 - beta)
   )
+  royston_D <- survival::royston(fit)
+  D <- D %||% royston_D[["D"]]
+  cens <- cens %||% (1 - fit$nevent / fit$n)
   lambda_m <- 2.66 + 1.26 * D ^ 1.9 - 1.65 * (D * cens) ^ 1.3 # Equation 3
   e2 <- lambda_m * (delta / zz) ^ -2 # Equation B2
   e <- lambda_m * (p * D / zz) ^ -2 # Equation 4
@@ -38,4 +44,17 @@ ssize_D <- function(D = 1.4, cens = 0.1, p = 0.1, alpha = 0.05, power = 0.8,
   # Take the lower of n2 or n (absolute and relative precision)
   n_final <- ceiling(pmin(n2, n))
   n_final
+}
+
+#' Convert R2 to D
+#'
+#' Convert R-Squared to D measure
+#'
+#' @param R2 R-squared
+#' @return The Royston & Sauerbrei D measure
+#' @author Derek Chiu
+R2_to_D <- function(R2) {
+  beta <- sqrt((R2 * pi ^ 2) / (6 * (1 - R2)))
+  D <- beta * sqrt(8 / pi)
+  D
 }
