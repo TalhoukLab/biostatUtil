40dedef6abab6326418dd2b5df04642b34060c10
Derek Chiu <dchiu@bccrc.ca>
2022-09-08 17:09:02 -0700

    add helper fun knit_with_date

---
+++ R/knit_with_date.R
@@ -0,0 +1,26 @@
+#' Knit R markdown report with today's date
+#'
+#' Append today's date to R markdown output file name. This function modifies
+#' the default behaviour of the Knit button in R Studio. It is intended to be
+#' passed in the YAML `knit:` field.
+#'
+#' @param input input file
+#' @param sep separator between input file name and date
+#' @param ... additional arguments to [rmarkdown::render()]
+#' @return The output file name will have today's date appended at the end.
+#'
+#' @author Derek Chiu
+#' @references https://bookdown.org/yihui/rmarkdown-cookbook/custom-knit.html
+#' @export
+knit_with_date <- (function(input, sep = "_", ...) {
+  if (!requireNamespace("rmarkdown", quietly = TRUE)) {
+    stop("Package \"rmarkdown\" is needed. Please install it.",
+         call. = FALSE)
+  } else {
+    rmarkdown::render(
+      input,
+      output_file = paste0(tools::file_path_sans_ext(input), sep, Sys.Date()),
+      envir = globalenv()
+    )
+  }
+})
